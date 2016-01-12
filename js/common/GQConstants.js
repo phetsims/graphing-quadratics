8403e1605c063ab85002abdfbc37bbdb367975e7
Chris Malley <cmalley@pixelzoom.com>
2016-01-12 14:54:03 -0700

    'expand' -> 'dilation', https://github.com/phetsims/sun/issues/204

--- js/common/GQConstants.js
+++ js/common/GQConstants.js
@@ -16,8 +16,7 @@ define( function( require ) {
     SCREEN_VIEW_OPTIONS: { layoutBounds: new Bounds2( 0, 0, 1100, 700 ) },
     X_AXIS_RANGE: new Range( -10, 10 ),
     Y_AXIS_RANGE: new Range( -10, 10 ),
-    INTERACTIVE_EQUATION_FONT_SIZE: 34,
-    PICKER_TOUCH_AREA_EXPAND_X: 30,
+    INTERACTIVE_EQUATION_FONT_SIZE: 34, PICKER_TOUCH_AREA_X_DILATION: 30,
     MANIPULATOR_RADIUS: 0.425
   };
 } );
