5f8fe2da19aa1adb13195d0df193c1e66e3d6b28
Andrea Lin <andrealin2018@gmail.com>
2018-07-10 15:03:24 -0600

    expand-collapse button functionality

--- js/common/view/EquationControls.js
+++ js/common/view/EquationControls.js
@@ -13,17 +13,25 @@ define( function( require ) {
   var BooleanProperty = require( 'AXON/BooleanProperty' );
   var Checkbox = require( 'SUN/Checkbox' );
   var EraserButton = require( 'SCENERY_PHET/buttons/EraserButton' );
+  var ExpandCollapseButton = require( 'SUN/ExpandCollapseButton' );
   var FontAwesomeNode = require( 'SUN/FontAwesomeNode' );
   var GQColors = require( 'GRAPHING_QUADRATICS/common/GQColors' );
   var graphingQuadratics = require( 'GRAPHING_QUADRATICS/graphingQuadratics' );
   var HBox = require( 'SCENERY/nodes/HBox' );
+  var HStrut = require( 'SCENERY/nodes/HStrut' );
   var inherit = require( 'PHET_CORE/inherit' );
+  var Line = require( 'SCENERY/nodes/Line' );
+  var Node = require( 'SCENERY/nodes/Node' );
   var Panel = require( 'SUN/Panel' );
   var PhetColorScheme = require( 'SCENERY_PHET/PhetColorScheme' );
   var RectangularPushButton = require( 'SUN/buttons/RectangularPushButton' );
+  var Text = require( 'SCENERY/nodes/Text' );
+  var VBox = require( 'SCENERY/nodes/VBox' );
 
   // constants
   var ICON_WIDTh = 30;
+  var Y_SPACING = 10;
+  var TITLE_X_SPACING = 5;
 
   /**
    * @constructor
@@ -35,6 +43,15 @@ define( function( require ) {
       xMargin: 10,
       yMargin: 10
     }, options );
+
+    // TODO: temporary. will be passed in as parameters.
+    var maximizedProperty = new BooleanProperty( true );
+    var titleNode = new Text( 'titleNode' );
+    var interactiveEquationNode = new Text( 'interactiveEquationNode');
+    var hideLinesProperty = new BooleanProperty( false );
+
+    // Expand/collapse button
+    var expandCollapseButton = new ExpandCollapseButton( maximizedProperty );
 
     // Save Line button
     var saveLineIcon = new FontAwesomeNode( 'camera', { maxWidth: ICON_WIDTh } );
@@ -48,7 +65,7 @@ define( function( require ) {
 
     // Hide Lines checkbox
     var hideLinesIcon = new FontAwesomeNode( 'eye_close', { maxWidth: ICON_WIDTh } );
-    var hideLinesCheckbox = new Checkbox( hideLinesIcon, new BooleanProperty( false ) );
+    var hideLinesCheckbox = new Checkbox( hideLinesIcon, hideLinesProperty );
 
     // horizontal layout of buttons
     var buttons = new HBox( {
@@ -56,7 +73,43 @@ define( function( require ) {
       spacing: 40
     } );
 
-    Panel.call( this, buttons, options );
+    var contentWidth = Math.max(
+      buttons.width,
+      interactiveEquationNode.width,
+      ( expandCollapseButton.width + titleNode.width + TITLE_X_SPACING )
+    );
+
+    // Stuff that is hidden when minimized must be attached to this node.
+    var separatorColor = 'rgb( 212, 212, 212 )';
+    var subContent = new VBox( {
+      spacing: Y_SPACING,
+      align: 'center',
+      children: [
+        new Line( 0, 0, contentWidth, 0, { stroke: separatorColor } ),
+        interactiveEquationNode,
+        new Line( 0, 0, contentWidth, 0, { stroke: separatorColor } ),
+        buttons
+      ]
+    } );
+
+    // Top-level content, with strut to prevent panel from resizing
+    var content = new Node( {
+      children: [ new HStrut( contentWidth ), expandCollapseButton, titleNode, subContent  ]
+    } );
+    titleNode.centerX = contentWidth / 2;
+    titleNode.centerY = expandCollapseButton.centerY;
+    subContent.top = Math.max( expandCollapseButton.bottom, titleNode.bottom ) + Y_SPACING;
+
+    maximizedProperty.link( function( maximized ) {
+      if ( maximized && content.indexOfChild( subContent ) === -1 ) {
+        content.addChild( subContent );
+      }
+      else if ( !maximized && content.indexOfChild( subContent ) !== -1 ) {
+        content.removeChild( subContent );
+      }
+    } );
+
+    Panel.call( this, content, options );
   }
 
   graphingQuadratics.register( 'EquationControls', EquationControls );
