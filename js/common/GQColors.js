220ada13996cb75da8ee2c8d736430a124614aff
Andrea Lin <andrealin2018@gmail.com>
2018-07-23 13:51:38 -0600

    create colors

--- js/common/GQColors.js
+++ js/common/GQColors.js
@@ -11,10 +11,21 @@ define( function( require ) {
 
   // modules
   const graphingQuadratics = require( 'GRAPHING_QUADRATICS/graphingQuadratics' );
+  const PhetColorScheme = require( 'SCENERY_PHET/PhetColorScheme' );
 
   const GQColors = {
     SCREEN_BACKGROUND: 'rgb( 238, 252, 252 )',
-    CONTROL_PANEL_BACKGROUND: 'rgb( 238, 238, 238 )'
+    CONTROL_PANEL_BACKGROUND: 'rgb( 238, 238, 238 )',
+    ACTIVE_CURVE: PhetColorScheme.RED_COLORBLIND,
+    SAVED_CURVE: 'blue',
+    QUADRATIC_TERM: 'hotpink',
+    LINEAR_TERM: 'green',
+    CONSTANT_TERM: 'black',
+    DIRECTRIX: 'green',
+    VERTEX: 'purple',
+    ROOTS: PhetColorScheme.RED_COLORBLIND,
+    A_SYMBOL: 'green',
+    INTERACTIVE_DIRECTRIX: PhetColorScheme.RED_COLORBLIND
   };
 
   graphingQuadratics.register( 'GQColors', GQColors );
