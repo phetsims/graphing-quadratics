674bdc283b5c0d1addae68b2b36f71c0c6f01f5e
Andrea Lin <andrealin2018@gmail.com>
2018-07-26 17:03:55 -0600

    convert to class

--- js/standardform/view/DecimalsGraphControls.js
+++ js/standardform/view/DecimalsGraphControls.js
@@ -13,7 +13,6 @@ define( function( require ) {
   const GQColors = require( 'GRAPHING_QUADRATICS/common/GQColors' );
   const GQFont = require( 'GRAPHING_QUADRATICS/common/GQFont' );
   const graphingQuadratics = require( 'GRAPHING_QUADRATICS/graphingQuadratics' );
-  const inherit = require( 'PHET_CORE/inherit' );
   const MathSymbols = require( 'SCENERY_PHET/MathSymbols' );
   const Panel = require( 'SUN/Panel' );
   const RichText = require( 'SCENERY/nodes/RichText' );
@@ -31,66 +30,67 @@ define( function( require ) {
   const xString = require( 'string!GRAPHING_QUADRATICS/x' );
   const yString = require( 'string!GRAPHING_QUADRATICS/y' );
 
-  /**
-   * @param {BooleanProperty} quadraticTermVisibleProperty
-   * @param {BooleanProperty} linearTermVisibleProperty
-   * @param {BooleanProperty} constantTermVisibleProperty
-   * @param {Object} [options]
-   * @constructor
-   */
-  function DecimalsGraphControls(
-    quadraticTermVisibleProperty,
-    linearTermVisibleProperty,
-    constantTermVisibleProperty,
-    options
-  ) {
+  class DecimalsGraphControls extends Panel {
 
-    options = _.extend( {
-      fill: GQColors.CONTROL_PANEL_BACKGROUND,
-      xMargin: 20,
-      yMargin: 15
-    }, options );
+    /**
+     * @param {BooleanProperty} quadraticTermVisibleProperty
+     * @param {BooleanProperty} linearTermVisibleProperty
+     * @param {BooleanProperty} constantTermVisibleProperty
+     * @param {Object} [options]
+     * @constructor
+     */
+    constructor(
+      quadraticTermVisibleProperty,
+      linearTermVisibleProperty,
+      constantTermVisibleProperty,
+      options
+    ) {
 
-    const quadraticTermEquation = new RichText( StringUtils.fillIn( '{{y}} {{equals}} {{a}}{{xSquared}}', {
-      xSquared: xSquaredString,
-      y: yString,
-      a: aString,
-      equals: MathSymbols.EQUAL_TO
-    } ), _.extend( {}, TEXT_OPTIONS, { fill: 'hotpink' } ) );
+      options = _.extend( {
+        fill: GQColors.CONTROL_PANEL_BACKGROUND,
+        xMargin: 20,
+        yMargin: 15
+      }, options );
 
-    const linearTermEquation = new RichText( StringUtils.fillIn( '{{y}} {{equals}} {{b}}{{x}}', {
-      x: xString,
-      y: yString,
-      b: bString,
-      equals: MathSymbols.EQUAL_TO
-    } ), _.extend( {}, TEXT_OPTIONS, { fill: 'green' } ) );
+      const quadraticTermEquation = new RichText( StringUtils.fillIn( '{{y}} {{equals}} {{a}}{{xSquared}}', {
+        xSquared: xSquaredString,
+        y: yString,
+        a: aString,
+        equals: MathSymbols.EQUAL_TO
+      } ), _.extend( {}, TEXT_OPTIONS, { fill: 'hotpink' } ) );
 
-    const constantTermEquation = new RichText( StringUtils.fillIn( '{{y}} {{equals}} {{c}}', {
-      y: yString,
-      c: cString,
-      equals: MathSymbols.EQUAL_TO
-    } ), _.extend( {}, TEXT_OPTIONS, { fill: 'black' } ) );
+      const linearTermEquation = new RichText( StringUtils.fillIn( '{{y}} {{equals}} {{b}}{{x}}', {
+        x: xString,
+        y: yString,
+        b: bString,
+        equals: MathSymbols.EQUAL_TO
+      } ), _.extend( {}, TEXT_OPTIONS, { fill: 'green' } ) );
 
-    // checkboxes that control visibility
-    const quadraticTermCheckbox = new Checkbox( quadraticTermEquation, quadraticTermVisibleProperty );
-    const linearTermCheckbox = new Checkbox( linearTermEquation, linearTermVisibleProperty );
-    const constantTermCheckbox = new Checkbox( constantTermEquation, constantTermVisibleProperty );
+      const constantTermEquation = new RichText( StringUtils.fillIn( '{{y}} {{equals}} {{c}}', {
+        y: yString,
+        c: cString,
+        equals: MathSymbols.EQUAL_TO
+      } ), _.extend( {}, TEXT_OPTIONS, { fill: 'black' } ) );
 
-    // vertical layout
-    const contentNode = new VBox( {
-      children: [
-        quadraticTermCheckbox,
-        linearTermCheckbox,
-        constantTermCheckbox
-      ],
-      spacing: 20,
-      align: 'left'
-    } );
+      // checkboxes that control visibility
+      const quadraticTermCheckbox = new Checkbox( quadraticTermEquation, quadraticTermVisibleProperty );
+      const linearTermCheckbox = new Checkbox( linearTermEquation, linearTermVisibleProperty );
+      const constantTermCheckbox = new Checkbox( constantTermEquation, constantTermVisibleProperty );
 
-    Panel.call( this, contentNode, options );
+      // vertical layout
+      const contentNode = new VBox( {
+        children: [
+          quadraticTermCheckbox,
+          linearTermCheckbox,
+          constantTermCheckbox
+        ],
+        spacing: 20,
+        align: 'left'
+      } );
+
+      super( contentNode, options );
+    }
   }
 
-  graphingQuadratics.register( 'DecimalsGraphControls', DecimalsGraphControls );
-
-  return inherit( Panel, DecimalsGraphControls );
+  return graphingQuadratics.register( 'DecimalsGraphControls', DecimalsGraphControls );
 } );
