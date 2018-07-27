cb911d91d613262f624d0e31e3686c43aecf62c9
Andrea Lin <andrealin2018@gmail.com>
2018-07-27 11:52:21 -0600

    convert to class

--- js/standardform/view/IntegersGraphControls.js
+++ js/standardform/view/IntegersGraphControls.js
@@ -13,7 +13,6 @@ define( function( require ) {
   const GQColors = require( 'GRAPHING_QUADRATICS/common/GQColors' );
   const GQFont = require( 'GRAPHING_QUADRATICS/common/GQFont' );
   const graphingQuadratics = require( 'GRAPHING_QUADRATICS/graphingQuadratics' );
-  const inherit = require( 'PHET_CORE/inherit' );
   const Panel = require( 'SUN/Panel' );
   const VBox = require( 'SCENERY/nodes/VBox' );
 
@@ -25,50 +24,50 @@ define( function( require ) {
   // constants
   const TEXT_OPTIONS = { font: new GQFont( 18 ) };
 
-  /**
-   * @param {BooleanProperty} vertexVisibleProperty
-   * @param {BooleanProperty} axisOfSymmetryVisibleProperty
-   * @param {BooleanProperty} rootsVisibleProperty
-   * @param {Object} [options]
-   * @constructor
-   */
-  function IntegersGraphControls(
-    vertexVisibleProperty,
-    axisOfSymmetryVisibleProperty,
-    rootsVisibleProperty,
-    options
-  ) {
+  class IntegersGraphControls extends Panel {
 
-    options = _.extend( {
-      fill: GQColors.CONTROL_PANEL_BACKGROUND,
-      xMargin: 20,
-      yMargin: 15
-    }, options );
+    /**
+     * @param {BooleanProperty} vertexVisibleProperty
+     * @param {BooleanProperty} axisOfSymmetryVisibleProperty
+     * @param {BooleanProperty} rootsVisibleProperty
+     * @param {Object} [options]
+     */
+    constructor(
+      vertexVisibleProperty,
+      axisOfSymmetryVisibleProperty,
+      rootsVisibleProperty,
+      options
+    ) {
 
-    // checkboxes that control visibility of vertex, axis of symmetry, and roots
-    const vertexCheckbox = Checkbox.createTextCheckbox( vertexString, TEXT_OPTIONS, vertexVisibleProperty );
-    const axisOfSymmetryCheckbox = Checkbox.createTextCheckbox(
-      axisOfSymmetryString,
-      TEXT_OPTIONS,
-      axisOfSymmetryVisibleProperty
-    );
-    const rootsCheckbox = Checkbox.createTextCheckbox( rootsString, TEXT_OPTIONS, rootsVisibleProperty );
+      options = _.extend( {
+        fill: GQColors.CONTROL_PANEL_BACKGROUND,
+        xMargin: 20,
+        yMargin: 15
+      }, options );
 
-    // vertical layout
-    const contentNode = new VBox( {
-      children: [
-        vertexCheckbox,
-        axisOfSymmetryCheckbox,
-        rootsCheckbox
-      ],
-      spacing: 20,
-      align: 'left'
-    } );
+      // checkboxes that control visibility of vertex, axis of symmetry, and roots
+      const vertexCheckbox = Checkbox.createTextCheckbox( vertexString, TEXT_OPTIONS, vertexVisibleProperty );
+      const axisOfSymmetryCheckbox = Checkbox.createTextCheckbox(
+        axisOfSymmetryString,
+        TEXT_OPTIONS,
+        axisOfSymmetryVisibleProperty
+      );
+      const rootsCheckbox = Checkbox.createTextCheckbox( rootsString, TEXT_OPTIONS, rootsVisibleProperty );
 
-    Panel.call( this, contentNode, options );
+      // vertical layout
+      const contentNode = new VBox( {
+        children: [
+          vertexCheckbox,
+          axisOfSymmetryCheckbox,
+          rootsCheckbox
+        ],
+        spacing: 20,
+        align: 'left'
+      } );
+
+      super( contentNode, options );
+    }
   }
 
-  graphingQuadratics.register( 'IntegersGraphControls', IntegersGraphControls );
-
-  return inherit( Panel, IntegersGraphControls );
+  return graphingQuadratics.register( 'IntegersGraphControls', IntegersGraphControls );
 } );
