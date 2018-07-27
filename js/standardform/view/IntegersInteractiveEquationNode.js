a6268c91dc809fba68e4765d85013f18981befad
Andrea Lin <andrealin2018@gmail.com>
2018-07-27 11:53:56 -0600

    convert to class

--- js/standardform/view/IntegersInteractiveEquationNode.js
+++ js/standardform/view/IntegersInteractiveEquationNode.js
@@ -12,7 +12,6 @@ define( function( require ) {
   // modules
   const GQFont = require( 'GRAPHING_QUADRATICS/common/GQFont' );
   const graphingQuadratics = require( 'GRAPHING_QUADRATICS/graphingQuadratics' );
-  const inherit = require( 'PHET_CORE/inherit' );
   const MathSymbols = require( 'SCENERY_PHET/MathSymbols' );
   const Node = require( 'SCENERY/nodes/Node' );
   const NumberPicker = require( 'SCENERY_PHET/NumberPicker' );
@@ -34,82 +33,82 @@ define( function( require ) {
     xMargin: 5
   };
 
-  /**
-   * @param {Property.<Quadratic|undefined>} quadraticProperty
-   * @param {Object} [options]
-   * @constructor
-   */
-  function IntegersInteractiveEquationNode( quadraticProperty, options ) {
+  class IntegersInteractiveEquationNode extends Node {
 
-    const aProperty = new NumberProperty( 0, { range: { min: -6, max: 6 } } ) ;
-    const bProperty = new NumberProperty( 0, { range: { min: -6, max: 6 } } ) ;
-    const cProperty = new NumberProperty( 0, { range: { min: -6, max: 6 } } ) ;
+    /**
+     * @param {Property.<Quadratic|undefined>} quadraticProperty
+     * @param {Object} [options]
+     */
+    constructor( quadraticProperty, options ) {
 
-    quadraticProperty.link( function( quadratic ) {
-      if ( quadratic.a !== aProperty.get() ) {
-        aProperty.set( quadratic.a );
-      }
-      if ( quadratic.b !== bProperty.get() ) {
-        bProperty.set( quadratic.b );
-      }
-      if ( quadratic.c !== cProperty.get() ) {
-        cProperty.set( quadratic.c );
-      }
-    } );
+      const aProperty = new NumberProperty( 0, { range: { min: -6, max: 6 } } );
+      const bProperty = new NumberProperty( 0, { range: { min: -6, max: 6 } } );
+      const cProperty = new NumberProperty( 0, { range: { min: -6, max: 6 } } );
 
-    Property.multilink( [ aProperty, bProperty, cProperty ], function( a, b, c ) {
-      const newQuadratic = new Quadratic( a, b, c );
-      if ( !newQuadratic.equals( quadraticProperty.get() ) ) {
-        quadraticProperty.set( new Quadratic( a, b, c ) );
-      }
-    } );
+      quadraticProperty.link( function( quadratic ) {
+        if ( quadratic.a !== aProperty.get() ) {
+          aProperty.set( quadratic.a );
+        }
+        if ( quadratic.b !== bProperty.get() ) {
+          bProperty.set( quadratic.b );
+        }
+        if ( quadratic.c !== cProperty.get() ) {
+          cProperty.set( quadratic.c );
+        }
+      } );
 
-    // interactive components of the equation
-    const aNumberPicker = new NumberPicker( aProperty, new Property( aProperty.range ), NUMBER_PICKER_OPTIONS );
-    const bNumberPicker = new NumberPicker( bProperty, new Property( bProperty.range ), NUMBER_PICKER_OPTIONS );
-    const cNumberPicker = new NumberPicker( cProperty, new Property( cProperty.range ), NUMBER_PICKER_OPTIONS );
+      Property.multilink( [ aProperty, bProperty, cProperty ], function( a, b, c ) {
+        const newQuadratic = new Quadratic( a, b, c );
+        if ( !newQuadratic.equals( quadraticProperty.get() ) ) {
+          quadraticProperty.set( new Quadratic( a, b, c ) );
+        }
+      } );
 
-    const yText = new RichText( yString, TEXT_OPTIONS );
-    const equalToText = new RichText( MathSymbols.EQUAL_TO, TEXT_OPTIONS );
-    const xSquaredText = new RichText( xSquaredString, TEXT_OPTIONS );
-    const plusText = new RichText( MathSymbols.PLUS, TEXT_OPTIONS );
-    const xText = new RichText( xString, TEXT_OPTIONS );
-    const secondPlusText = new RichText( MathSymbols.PLUS, TEXT_OPTIONS );
+      // interactive components of the equation
+      const aNumberPicker = new NumberPicker( aProperty, new Property( aProperty.range ), NUMBER_PICKER_OPTIONS );
+      const bNumberPicker = new NumberPicker( bProperty, new Property( bProperty.range ), NUMBER_PICKER_OPTIONS );
+      const cNumberPicker = new NumberPicker( cProperty, new Property( cProperty.range ), NUMBER_PICKER_OPTIONS );
 
-    Node.call( this, {
-      children: [
-        yText,
-        equalToText,
-        aNumberPicker,
-        xSquaredText,
-        plusText,
-        bNumberPicker,
-        xText,
-        secondPlusText,
-        cNumberPicker
-      ]
-    } );
+      const yText = new RichText( yString, TEXT_OPTIONS );
+      const equalToText = new RichText( MathSymbols.EQUAL_TO, TEXT_OPTIONS );
+      const xSquaredText = new RichText( xSquaredString, TEXT_OPTIONS );
+      const plusText = new RichText( MathSymbols.PLUS, TEXT_OPTIONS );
+      const xText = new RichText( xString, TEXT_OPTIONS );
+      const secondPlusText = new RichText( MathSymbols.PLUS, TEXT_OPTIONS );
 
-    // alignment
-    equalToText.left = yText.right + 10;
-    aNumberPicker.left = equalToText.right + 10;
-    xSquaredText.left = aNumberPicker.right + 10;
-    plusText.left = xSquaredText.right + 10;
-    bNumberPicker.left = plusText.right + 10;
-    xText.left = bNumberPicker.right + 10;
-    secondPlusText.left = xText.right + 10;
-    cNumberPicker.left = secondPlusText.right + 10;
-    equalToText.bottom = yText.bottom;
-    xSquaredText.bottom = yText.bottom;
-    plusText.bottom = yText.bottom;
-    xText.bottom = yText.bottom;
-    secondPlusText.bottom = yText.bottom;
-    aNumberPicker.centerY = xText.centerY;
-    bNumberPicker.centerY = xText.centerY;
-    cNumberPicker.centerY = xText.centerY;
+      super ( {
+        children: [
+          yText,
+          equalToText,
+          aNumberPicker,
+          xSquaredText,
+          plusText,
+          bNumberPicker,
+          xText,
+          secondPlusText,
+          cNumberPicker
+        ]
+      } );
+
+      // alignment
+      equalToText.left = yText.right + 10;
+      aNumberPicker.left = equalToText.right + 10;
+      xSquaredText.left = aNumberPicker.right + 10;
+      plusText.left = xSquaredText.right + 10;
+      bNumberPicker.left = plusText.right + 10;
+      xText.left = bNumberPicker.right + 10;
+      secondPlusText.left = xText.right + 10;
+      cNumberPicker.left = secondPlusText.right + 10;
+      equalToText.bottom = yText.bottom;
+      xSquaredText.bottom = yText.bottom;
+      plusText.bottom = yText.bottom;
+      xText.bottom = yText.bottom;
+      secondPlusText.bottom = yText.bottom;
+      aNumberPicker.centerY = xText.centerY;
+      bNumberPicker.centerY = xText.centerY;
+      cNumberPicker.centerY = xText.centerY;
+    }
   }
 
-  graphingQuadratics.register( 'IntegersInteractiveEquationNode', IntegersInteractiveEquationNode );
-
-  return inherit( Node, IntegersInteractiveEquationNode );
+  return graphingQuadratics.register( 'IntegersInteractiveEquationNode', IntegersInteractiveEquationNode );
 } );
