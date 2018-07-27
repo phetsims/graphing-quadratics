d312227cff2a9abed07b2526b155f01f72bfa136
Andrea Lin <andrealin2018@gmail.com>
2018-07-27 11:50:26 -0600

    convert to class

--- js/standardform/view/DecimalsInteractiveEquationNode.js
+++ js/standardform/view/DecimalsInteractiveEquationNode.js
@@ -12,8 +12,6 @@ define( function( require ) {
   // modules
   const GQFont = require( 'GRAPHING_QUADRATICS/common/GQFont' );
   const graphingQuadratics = require( 'GRAPHING_QUADRATICS/graphingQuadratics' );
-  const HBox = require( 'SCENERY/nodes/HBox' );
-  const inherit = require( 'PHET_CORE/inherit' );
   const MathSymbols = require( 'SCENERY_PHET/MathSymbols' );
   const Node = require( 'SCENERY/nodes/Node' );
   const NumberDisplay = require( 'SCENERY_PHET/NumberDisplay' );
@@ -42,98 +40,98 @@ define( function( require ) {
     yMargin: 0
   };
 
-  /**
-   * @param {Property.<Quadratic|undefined>} quadraticProperty
-   * @param {Object} [options]
-   * @constructor
-   */
-  function DecimalsInteractiveEquationNode( quadraticProperty, options ) {
+  class DecimalsInteractiveEquationNode extends Node {
 
-    const aProperty = new NumberProperty( 1, { range: { min: -6, max: 6 } } ) ;
-    const bProperty = new NumberProperty( 0, { range: { min: -6, max: 6 } } ) ;
-    const cProperty = new NumberProperty( 0, { range: { min: -6, max: 6 } } ) ;
+    /**
+     * @param {Property.<Quadratic|undefined>} quadraticProperty
+     * @param {Object} [options]
+     */
+    constructor( quadraticProperty, options ) {
 
-    quadraticProperty.link( function( quadratic ) {
-      if ( quadratic.a !== aProperty.get() ) {
-        aProperty.set( quadratic.a );
-      }
-      if ( quadratic.b !== bProperty.get() ) {
-        bProperty.set( quadratic.b );
-      }
-      if ( quadratic.c !== cProperty.get() ) {
-        cProperty.set( quadratic.c );
-      }
-    } );
+      const aProperty = new NumberProperty( 1, { range: { min: -6, max: 6 } } );
+      const bProperty = new NumberProperty( 0, { range: { min: -6, max: 6 } } );
+      const cProperty = new NumberProperty( 0, { range: { min: -6, max: 6 } } );
 
-    Property.multilink( [ aProperty, bProperty, cProperty ], function( a, b, c ) {
-      const newQuadratic = new Quadratic( a, b, c );
-      if ( !newQuadratic.equals( quadraticProperty.get() ) ) {
-        quadraticProperty.set( new Quadratic( a, b, c ) );
-      }
-    } );
+      quadraticProperty.link( function( quadratic ) {
+        if ( quadratic.a !== aProperty.get() ) {
+          aProperty.set( quadratic.a );
+        }
+        if ( quadratic.b !== bProperty.get() ) {
+          bProperty.set( quadratic.b );
+        }
+        if ( quadratic.c !== cProperty.get() ) {
+          cProperty.set( quadratic.c );
+        }
+      } );
 
-    const aReadout = new NumberDisplay(
-      aProperty,
-      aProperty.range,
-      _.extend( {}, READOUT_OPTIONS, { decimalPlaces: 2 } )
-    );
-    const bReadout = new NumberDisplay( bProperty, bProperty.range, READOUT_OPTIONS );
-    const cReadout = new NumberDisplay( cProperty, bProperty.range, READOUT_OPTIONS );
+      Property.multilink( [ aProperty, bProperty, cProperty ], function( a, b, c ) {
+        const newQuadratic = new Quadratic( a, b, c );
+        if ( !newQuadratic.equals( quadraticProperty.get() ) ) {
+          quadraticProperty.set( new Quadratic( a, b, c ) );
+        }
+      } );
 
-    const yText = new RichText( yString, TEXT_OPTIONS );
-    const equalToText = new RichText( MathSymbols.EQUAL_TO, TEXT_OPTIONS );
-    const xSquaredText = new RichText( xSquaredString, TEXT_OPTIONS );
-    const plusText = new RichText( MathSymbols.PLUS, TEXT_OPTIONS );
-    const xText = new RichText( xString, TEXT_OPTIONS );
-    const secondPlusText = new RichText( MathSymbols.PLUS, TEXT_OPTIONS );
+      const aReadout = new NumberDisplay(
+        aProperty,
+        aProperty.range,
+        _.extend( {}, READOUT_OPTIONS, { decimalPlaces: 2 } )
+      );
+      const bReadout = new NumberDisplay( bProperty, bProperty.range, READOUT_OPTIONS );
+      const cReadout = new NumberDisplay( cProperty, bProperty.range, READOUT_OPTIONS );
 
-    const aControl = new SliderUnit( aString, aProperty, 2 );
-    const bControl = new SliderUnit( bString, bProperty, 1 );
-    const cControl = new SliderUnit( cString, cProperty, 1 );
+      const yText = new RichText( yString, TEXT_OPTIONS );
+      const equalToText = new RichText( MathSymbols.EQUAL_TO, TEXT_OPTIONS );
+      const xSquaredText = new RichText( xSquaredString, TEXT_OPTIONS );
+      const plusText = new RichText( MathSymbols.PLUS, TEXT_OPTIONS );
+      const xText = new RichText( xString, TEXT_OPTIONS );
+      const secondPlusText = new RichText( MathSymbols.PLUS, TEXT_OPTIONS );
 
-    Node.call( this, {
-      children: [
-        yText,
-        equalToText,
-        aReadout,
-        xSquaredText,
-        plusText,
-        bReadout,
-        xText,
-        secondPlusText,
-        cReadout,
-        aControl,
-        bControl,
-        cControl
-      ]
-    } );
+      const aControl = new SliderUnit( aString, aProperty, 2 );
+      const bControl = new SliderUnit( bString, bProperty, 1 );
+      const cControl = new SliderUnit( cString, cProperty, 1 );
 
-    // alignment
-    equalToText.left = yText.right + 10;
-    aReadout.left = equalToText.right + 10;
-    xSquaredText.left = aReadout.right + 5;
-    plusText.left = xSquaredText.right + 10;
-    bReadout.left = plusText.right + 10;
-    xText.left = bReadout.right + 5;
-    secondPlusText.left = xText.right + 10;
-    cReadout.left = secondPlusText.right + 10;
-    equalToText.bottom = yText.bottom;
-    xSquaredText.bottom = yText.bottom;
-    plusText.bottom = yText.bottom;
-    xText.bottom = yText.bottom;
-    secondPlusText.bottom = yText.bottom;
-    aReadout.bottom = yText.bottom;
-    bReadout.bottom = yText.bottom;
-    cReadout.bottom = yText.bottom;
-    aControl.centerX = aReadout.centerX;
-    bControl.centerX = bReadout.centerX;
-    cControl.centerX = cReadout.centerX;
-    aControl.top = aReadout.bottom + 5;
-    bControl.top = bReadout.bottom + 5;
-    cControl.top = cReadout.bottom + 5;
+      super( {
+        children: [
+          yText,
+          equalToText,
+          aReadout,
+          xSquaredText,
+          plusText,
+          bReadout,
+          xText,
+          secondPlusText,
+          cReadout,
+          aControl,
+          bControl,
+          cControl
+        ]
+      } );
+
+      // alignment
+      equalToText.left = yText.right + 10;
+      aReadout.left = equalToText.right + 10;
+      xSquaredText.left = aReadout.right + 5;
+      plusText.left = xSquaredText.right + 10;
+      bReadout.left = plusText.right + 10;
+      xText.left = bReadout.right + 5;
+      secondPlusText.left = xText.right + 10;
+      cReadout.left = secondPlusText.right + 10;
+      equalToText.bottom = yText.bottom;
+      xSquaredText.bottom = yText.bottom;
+      plusText.bottom = yText.bottom;
+      xText.bottom = yText.bottom;
+      secondPlusText.bottom = yText.bottom;
+      aReadout.bottom = yText.bottom;
+      bReadout.bottom = yText.bottom;
+      cReadout.bottom = yText.bottom;
+      aControl.centerX = aReadout.centerX;
+      bControl.centerX = bReadout.centerX;
+      cControl.centerX = cReadout.centerX;
+      aControl.top = aReadout.bottom + 5;
+      bControl.top = bReadout.bottom + 5;
+      cControl.top = cReadout.bottom + 5;
+    }
   }
 
-  graphingQuadratics.register( 'DecimalsInteractiveEquationNode', DecimalsInteractiveEquationNode );
-
-  return inherit( HBox, DecimalsInteractiveEquationNode );
+  return graphingQuadratics.register( 'DecimalsInteractiveEquationNode', DecimalsInteractiveEquationNode );
 } );
