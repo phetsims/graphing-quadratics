0b19e9112476624b2b05afe9cc4f61ee1ded300f
Chris Malley <cmalley@pixelzoom.com>
2018-08-14 10:44:09 -0600

    maxWidth for control panels
    Signed-off-by: Chris Malley <cmalley@pixelzoom.com>

--- js/common/view/GQSceneNode.js
+++ js/common/view/GQSceneNode.js
@@ -18,6 +18,9 @@ define( function( require ) {
   const PointToolNode = require( 'GRAPHING_QUADRATICS/common/view/PointToolNode' );
   const VBox = require( 'SCENERY/nodes/VBox' );
 
+  // constants
+  const X_SPACING = 15; // between graph and control panels
+
   class GQSceneNode extends Node {
 
     /**
@@ -57,6 +60,8 @@ define( function( require ) {
         hasVertexManipulator: options.hasVertexManipulator
       } );
 
+      const controlPanelMaxWidth = layoutBounds.width - graphNode.width - ( 2 * GQConstants.SCREEN_VIEW_X_MARGIN ) - X_SPACING;
+
       // Interactive equation and associated controls. dispose not needed.
       const equationAccordionBox = new EquationAccordionBox(
         interactiveEquationNode,
@@ -64,12 +69,14 @@ define( function( require ) {
         scene.eraseQuadratics.bind( scene ),
         scene.savedQuadratics.lengthProperty, {
           titleNode: accordionBoxTitleNode,
-          expandedProperty: viewProperties.interactiveEquationVisibleProperty
+          expandedProperty: viewProperties.interactiveEquationVisibleProperty,
+          maxWidth: controlPanelMaxWidth
         }
       );
 
       // Parent for all control panels, to simplify layout
       const controlsParent = new VBox( {
+        maxWidth: controlPanelMaxWidth,
         resize: false,
         align: 'center',
         spacing: 15,
@@ -84,12 +91,8 @@ define( function( require ) {
       this.addChild( graphNode );
       this.addChild( pointToolsParent );
 
-      // Constrain control panels to amount of horizontal space available.
-      const availableControlPanelWidth = layoutBounds.width - graphNode.right - ( 2 * GQConstants.SCREEN_VIEW_X_MARGIN );
-      controlsParent.maxWidth = 0.9 * availableControlPanelWidth;
-
       // Horizontally center controls in the space to the right of the graph.
-      controlsParent.centerX = graphNode.right + GQConstants.SCREEN_VIEW_X_MARGIN + ( availableControlPanelWidth / 2 );
+      controlsParent.centerX = graphNode.right + X_SPACING + ( controlPanelMaxWidth / 2 );
       controlsParent.top = GQConstants.SCREEN_VIEW_Y_MARGIN;
 
       // @public
