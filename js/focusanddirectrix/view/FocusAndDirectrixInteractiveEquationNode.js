8791f4834767f27ddaf36404df7f95b32853f0dc
Chris Malley <cmalley@pixelzoom.com>
2018-09-19 11:53:50 -0600

    add sliders to F&D
    Signed-off-by: Chris Malley <cmalley@pixelzoom.com>

--- js/focusanddirectrix/view/FocusAndDirectrixInteractiveEquationNode.js
+++ js/focusanddirectrix/view/FocusAndDirectrixInteractiveEquationNode.js
@@ -2,34 +2,57 @@
 
 /**
  * Interactive equation for the 'Focus & Directrix' screen.
- * 
+ *
  * @author Chris Malley (PixelZoom, Inc.)
  */
 define( require => {
   'use strict';
 
   // modules
+  const CoefficientSlider = require( 'GRAPHING_QUADRATICS/common/view/CoefficientSlider' );
+  const GQColors = require( 'GRAPHING_QUADRATICS/common/GQColors' );
+  const GQSymbols = require( 'GRAPHING_QUADRATICS/common/GQSymbols' );
   const graphingQuadratics = require( 'GRAPHING_QUADRATICS/graphingQuadratics' );
+  const HBox = require( 'SCENERY/nodes/HBox' );
   const Node = require( 'SCENERY/nodes/Node' );
-  const Rectangle = require( 'SCENERY/nodes/Rectangle' );
+  const NumberProperty = require( 'AXON/NumberProperty' );
 
   class FocusAndDirectrixInteractiveEquationNode extends Node {
 
     /**
      * @param {Property.<Quadratic|undefined>} quadraticProperty
-     * @param {RangeWithValue} hRange
      * @param {RangeWithValue} pRange
+     * @param {RangeWithValue} hRange
      * @param {RangeWithValue} kRange
      * @param {Object} [options]
      */
-    constructor( quadraticProperty, hRange, pRange, kRange, options ) {
+    constructor( quadraticProperty, pRange, hRange, kRange, options ) {
 
       options = options || {};
 
-      var placeholder = new Rectangle( 0, 0, 300, 250, { stroke: 'red' } );
+      // coefficient Properties
+      const pProperty = new NumberProperty( pRange.defaultValue, { range: pRange } );
+      const hProperty = new NumberProperty( hRange.defaultValue, { range: hRange } );
+      const kProperty = new NumberProperty( kRange.defaultValue, { range: kRange } );
+
+      // coefficient sliders
+      const pSlider = new CoefficientSlider( GQSymbols.p, pProperty, {
+        labelColor: GQColors.FOCUS_AND_DIRECTRIX_P
+      } );
+      const hSlider = new CoefficientSlider( GQSymbols.h, hProperty, {
+        labelColor: GQColors.FOCUS_AND_DIRECTRIX_H
+      } );
+      const kSlider = new CoefficientSlider( GQSymbols.k, kProperty, {
+        labelColor: GQColors.FOCUS_AND_DIRECTRIX_K
+      } );
+
+      var hBox = new HBox( {
+        spacing: 40,
+        children: [ pSlider, hSlider, kSlider ]
+      } );
 
       assert && assert( !options.children, 'FocusAndDirectrixInteractiveEquationNode sets children' );
-      options.children = [ placeholder ];
+      options.children = [ hBox ];
 
       super( options );
     }
