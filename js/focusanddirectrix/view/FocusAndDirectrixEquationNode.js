42639944b72eb37487186e3fbaa6f1e92c54dfa1
Chris Malley <cmalley@pixelzoom.com>
2018-09-18 18:09:25 -0600

    fix up equation in F&D screen
    Signed-off-by: Chris Malley <cmalley@pixelzoom.com>

--- js/focusanddirectrix/view/FocusAndDirectrixEquationNode.js
+++ js/focusanddirectrix/view/FocusAndDirectrixEquationNode.js
@@ -12,12 +12,15 @@ define( require => {
   // modules
   const GQSymbols = require( 'GRAPHING_QUADRATICS/common/GQSymbols' );
   const graphingQuadratics = require( 'GRAPHING_QUADRATICS/graphingQuadratics' );
+  const Line = require( 'SCENERY/nodes/Line' );
   const MathSymbols = require( 'SCENERY_PHET/MathSymbols' );
+  const Node = require( 'SCENERY/nodes/Node' );
   const PhetFont = require( 'SCENERY_PHET/PhetFont' );
   const RichText = require( 'SCENERY/nodes/RichText' );
   const StringUtils = require( 'PHETCOMMON/util/StringUtils' );
+  const VBox = require( 'SCENERY/nodes/VBox' );
 
-  class FocusAndDirectrixEquationNode extends RichText {
+  class FocusAndDirectrixEquationNode extends Node {
 
     /**
      * @param {Object} [options]
@@ -25,25 +28,71 @@ define( require => {
     constructor( options ) {
 
       options = _.extend( {
-
-        // RichText options
         font: new PhetFont( 26 ),
-        fill: 'black'
+        fractionFont: new PhetFont( 22 ),
+        color: 'black'
       } );
 
-      const equation = StringUtils.fillIn(
-        '{{y}} {{equals}} 1/4{{p}} ({{x}} {{minus}} {{h}})<sup>2</sup> {{plus}} {{k}}', {
-          x: GQSymbols.x,
-          y: GQSymbols.y,
-          h: GQSymbols.h,
-          k: GQSymbols.k,
-          p: GQSymbols.p,
-          equals: MathSymbols.EQUAL_TO,
-          plus: MathSymbols.PLUS,
-          minus: MathSymbols.MINUS
-        } );
+      // y =
+      const leftString = StringUtils.fillIn( '{{y}} {{equals}}', {
+        y: GQSymbols.y,
+        equals: MathSymbols.EQUAL_TO
+      } );
+      const leftNode = new RichText( leftString, {
+        font: options.font,
+        fill: options.color
+      } );
 
-      super( equation, options );
+      // 1
+      const numeratorNode = new RichText( '1', {
+        font: options.fractionFont,
+        fill: options.color
+      } );
+
+      // 4p
+      const denominatorString = StringUtils.fillIn( '4{{p}}', {
+        p: GQSymbols.p
+      } );
+      const denominatorNode = new RichText( denominatorString, {
+        font: options.fractionFont,
+        fill: options.color
+      } );
+
+      // horizontal line between numerator and denominator
+      const fractionLineLength = 1.25 * Math.max( numeratorNode.width, denominatorNode.width );
+      const fractionLine = new Line( 0, 0, fractionLineLength, 0, {
+        stroke: options.color,
+        lineWidth: 1
+      } );
+
+      // 1/4p
+      const fractionNode = new VBox( {
+        spacing: 2,
+        children: [ numeratorNode, fractionLine, denominatorNode ]
+      } );
+
+      // (x - h)^2 + k
+      const rightString = StringUtils.fillIn( '({{x}} {{minus}} {{h}})<sup>2</sup> {{plus}} {{k}}', {
+        x: GQSymbols.x,
+        h: GQSymbols.h,
+        k: GQSymbols.k,
+        plus: MathSymbols.PLUS,
+        minus: MathSymbols.MINUS
+      } );
+      const rightNode = new RichText( rightString, {
+        font: options.font,
+        fill: options.color
+      } );
+
+      assert && assert( !options.children, 'FocusAndDirectrixEquationNode sets children' );
+      options.children = [ leftNode, fractionNode, rightNode ];
+
+      const xSpacing = 5;
+      fractionNode.left = leftNode.right + xSpacing;
+      fractionNode.centerY = leftNode.centerY;
+      rightNode.left = fractionNode.right + xSpacing;
+
+      super( options );
     }
   }
 
