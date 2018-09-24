164ea60a963e5dbc0912ef1378c172ec844e4baf
Chris Malley <cmalley@pixelzoom.com>
2018-09-24 13:29:44 -0600

    split into 2 lines, so that visibility can be controlled independently
    Signed-off-by: Chris Malley <cmalley@pixelzoom.com>

--- js/focusanddirectrix/view/PointOnQuadraticLinesNode.js
+++ js/focusanddirectrix/view/PointOnQuadraticLinesNode.js
@@ -2,6 +2,7 @@
 
 /**
  * Dashed lines that connect the 'point on quadratic' to the focus and directrix.
+ * This is implemented as 2 Lines (rather than 1 Path) so that their visibility can be controlled independently.
  *
  * @author Chris Malley (PixelZoom, Inc.)
  */
@@ -12,10 +13,9 @@ define( require => {
   const GQColors = require( 'GRAPHING_QUADRATICS/common/GQColors' );
   const GQConstants = require( 'GRAPHING_QUADRATICS/common/GQConstants' );
   const graphingQuadratics = require( 'GRAPHING_QUADRATICS/graphingQuadratics' );
+  const Line = require( 'SCENERY/nodes/Line' );
   const Node = require( 'SCENERY/nodes/Node' );
-  const Path = require( 'SCENERY/nodes/Path' );
   const Property = require( 'AXON/Property' );
-  const Shape = require( 'KITE/Shape' );
 
   /**
    * @param {Property.<Quadratic>} quadraticProperty
@@ -30,13 +30,19 @@ define( require => {
     constructor( quadraticProperty, pointOnQuadraticProperty, modelViewTransform,
                  pointOnQuadraticVisibleProperty, focusVisibleProperty, directrixVisibleProperty ) {
 
-      const path = new Path( null, {
+      const pathOptions = {
         stroke: GQColors.POINT_ON_QUADRATIC,
         lineWidth: GQConstants.POINT_ON_QUADRATIC_LINE_WIDTH,
         lineDash: GQConstants.POINT_ON_QUADRATIC_LINE_DASH
-      } );
-      
-      super( { children: [ path ] } );
+      };
+
+      // line connecting point and focus
+      const focusLine = new Line( 0, 0, 0, 1, pathOptions );
+
+      // line connecting point and directrix                                             
+      const directrixLine = new Line( 0, 0, 0, 1, pathOptions );
+
+      super( { children: [ focusLine, directrixLine ] } );
 
       // update the lines
       Property.multilink( [ quadraticProperty, pointOnQuadraticProperty ],
@@ -45,15 +51,19 @@ define( require => {
           assert && assert( quadratic.focus !== undefined, 'undefined focus is not supported' );
           assert && assert( quadratic.directrix !== undefined, 'undefined directrix is not supported' );
 
-          path.shape = new Shape()
-            .moveToPoint( modelViewTransform.modelToViewPosition( quadratic.focus ) )
-            .lineToPoint( modelViewTransform.modelToViewPosition( pointOnQuadratic ) )
-            .lineTo( modelViewTransform.modelToViewX( pointOnQuadratic.x ), modelViewTransform.modelToViewY( quadratic.directrix ) );
+          const pointView = modelViewTransform.modelToViewPosition( pointOnQuadratic );
+          const focusView = modelViewTransform.modelToViewPosition( quadratic.focus );
+          const directrixView = modelViewTransform.modelToViewY( quadratic.directrix );
+
+          focusLine.setLine( pointView.x, pointView.y, focusView.x, focusView.y );
+          directrixLine.setLine( pointView.x, pointView.y, pointView.x, directrixView );
         } );
 
+      // visibility
       Property.multilink( [ pointOnQuadraticVisibleProperty, focusVisibleProperty, directrixVisibleProperty ],
         ( pointOnQuadraticVisible, focusVisibleProperty, directrixVisible ) => {
-        path.visible = ( pointOnQuadraticVisible && focusVisibleProperty && directrixVisible );
+          focusLine.visible = ( pointOnQuadraticVisible && focusVisibleProperty && directrixVisible );
+          directrixLine.visible = ( pointOnQuadraticVisible && focusVisibleProperty && directrixVisible );
         } );
     }
   }
