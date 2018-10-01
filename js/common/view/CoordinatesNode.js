e0d34724f29f03525db245f42e342837d4667ea8
Chris Malley <cmalley@pixelzoom.com>
2018-10-01 14:56:51 -0600

    remove TODO
    Signed-off-by: Chris Malley <cmalley@pixelzoom.com>

--- js/common/view/CoordinatesNode.js
+++ js/common/view/CoordinatesNode.js
@@ -21,7 +21,6 @@ define( require => {
   const coordinateUnknownString = require( 'string!GRAPHING_QUADRATICS/coordinateUnknown' );
   const pointXYString = require( 'string!GRAPHING_QUADRATICS/pointXY' );
 
-  //TODO #14 instrument CoordinatesNode?
   class CoordinatesNode extends Node {
 
     /**
