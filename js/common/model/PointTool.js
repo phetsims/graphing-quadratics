205800ecf46ade4a534447675de2a2b0271d0a90
Chris Malley <cmalley@pixelzoom.com>
2018-10-01 11:07:42 -0600

    make onQuadraticProperty a DerivedProperty
    Signed-off-by: Chris Malley <cmalley@pixelzoom.com>

--- js/common/model/PointTool.js
+++ js/common/model/PointTool.js
@@ -9,10 +9,11 @@ define( require => {
   'use strict';
 
   // modules
+  const DerivedProperty = require( 'AXON/DerivedProperty' );
+  const DerivedPropertyIO = require( 'AXON/DerivedPropertyIO' );
   const graphingQuadratics = require( 'GRAPHING_QUADRATICS/graphingQuadratics' );
   const Property = require( 'AXON/Property' );
   const PropertyIO = require( 'AXON/PropertyIO' );
-  const Quadratic = require( 'GRAPHING_QUADRATICS/common/model/Quadratic' );
   const QuadraticIO = require( 'GRAPHING_QUADRATICS/common/model/QuadraticIO' );
   const Tandem = require( 'TANDEM/Tandem' );
   const Vector2 = require( 'DOT/Vector2' );
@@ -35,7 +36,7 @@ define( require => {
       options = _.extend( {
         location: Vector2.ZERO, // {Vector2} initial location
         probeLocation: 'left', // {string} which side the probe is on, see PROBE_LOCATIONS
-        dragBounds: null, // {Bounds2|null} drag bounds in model coordinate frame
+        dragBounds: null, // {Bounds2|null} drag bounds, in model coordinate frame
         tandem: Tandem.required
       }, options );
 
@@ -52,38 +53,29 @@ define( require => {
 
       // @public (read-only)
       this.quadratics = quadratics;
+      this.probeLocation = options.probeLocation;
+      this.dragBounds = options.dragBounds;
 
-      // @public quadratic that the tool is on, null if it's not on a quadratic
-      this.onQuadraticProperty = new Property( null, {
-        isValidValue: value => { return value instanceof Quadratic || value === null; },
-        tandem: options.tandem.createTandem( 'onQuadraticProperty' ),
-        phetioType: PropertyIO( NullableIO( QuadraticIO ) ),
-        phetioInstanceDocumentation: 'the quadratic that this point tool is on, null if not on a quadratic'
-      } );
-
-      this.probeLocation = options.probeLocation; // @public
-      this.dragBounds = options.dragBounds; // @public
-
-      //TODO should onQuadraticProperty be a DerivedProperty?
-      // Update when the point tool moves or the quadratics change.
-      Property.multilink( [ this.locationProperty, quadratics.lengthProperty ],
+      // @public
+      this.onQuadraticProperty = new DerivedProperty( [ this.locationProperty, quadratics.lengthProperty ],
         ( location, length ) => {
           for ( let i = 0; i < length; i++ ) {
-            var quadratic = quadratics.get( i );
+            let quadratic = quadratics.get( i );
             if ( quadratic.isOnQuadratic( this.locationProperty.value ) ) {
-              this.onQuadraticProperty.value = quadratic;
-              return;
+              return quadratic;
             }
           }
-          this.onQuadraticProperty.value = null;
-        }
-      );
+          return null;
+        }, {
+          tandem: options.tandem.createTandem( 'onQuadraticProperty' ),
+          phetioType: DerivedPropertyIO( NullableIO( QuadraticIO ) ),
+          phetioInstanceDocumentation: 'the quadratic that this point tool is on, null if not on a quadratic'
+        } );
     }
 
     // @public
     reset() {
       this.locationProperty.reset();
-      this.onQuadraticProperty.reset();
     }
   }
 
