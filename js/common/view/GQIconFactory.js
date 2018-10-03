e932e9a0b366a082bcef88c65a2c266fb4df2336
Chris Malley <cmalley@pixelzoom.com>
2018-10-03 11:05:23 -0600

    placeholders for screen icons
    Signed-off-by: Chris Malley <cmalley@pixelzoom.com>

--- js/common/view/GQIconFactory.js
+++ js/common/view/GQIconFactory.js
@@ -10,7 +10,22 @@ define( require => {
   'use strict';
 
   // modules
+  const BooleanProperty = require( 'AXON/BooleanProperty' );
+  const DirectrixNode = require( 'GRAPHING_QUADRATICS/focusanddirectrix/view/DirectrixNode' );
+  const GQColors = require( 'GRAPHING_QUADRATICS/common/GQColors' );
+  const GQSymbols = require( 'GRAPHING_QUADRATICS/common/GQSymbols' );
   const graphingQuadratics = require( 'GRAPHING_QUADRATICS/graphingQuadratics' );
+  const HBox = require( 'SCENERY/nodes/HBox' );
+  const Manipulator = require( 'GRAPHING_LINES/common/view/manipulator/Manipulator' );
+  const ModelViewTransform2 = require( 'PHETCOMMON/view/ModelViewTransform2' );
+  const PhetFont = require( 'SCENERY_PHET/PhetFont' );
+  const Property = require( 'AXON/Property' );
+  const Quadratic = require( 'GRAPHING_QUADRATICS/common/model/Quadratic' );
+  const QuadraticNode = require( 'GRAPHING_QUADRATICS/common/view/QuadraticNode' );
+  const Range = require( 'DOT/Range' );
+  const RichText = require( 'SCENERY/nodes/RichText' );
+  const ScreenIcon = require( 'JOIST/ScreenIcon' );
+  const VBox = require( 'SCENERY/nodes/VBox' );
 
   const GQIconFactory = {
 
@@ -19,7 +34,25 @@ define( require => {
      * @returns {Node}
      */
     createExploreScreenIcon() {
-      return null;
+
+      const quadratic = new Quadratic( -0.05, 0, 0, {
+        color: GQColors.EXPLORE_INTERACTIVE_CURVE
+      } );
+
+      const quadraticNode = new QuadraticNode(
+        new Property( quadratic ),
+        new Range( -50, 50 ),
+        new Range( -50, 50 ),
+        ModelViewTransform2.createIdentity(),
+        'standard',
+        new BooleanProperty( false ), {
+          lineWidth: 5
+        } );
+
+      return new ScreenIcon( quadraticNode, {
+        fill: GQColors.SCREEN_BACKGROUND,
+        maxIconWidthProportion: 0.7
+      } );
     },
 
     /**
@@ -27,7 +60,33 @@ define( require => {
      * @returns {Node}
      */
     createStandardFormScreenIcon() {
-      return null;
+
+      const font = new PhetFont( { size: 40, weight: 'bold' } );
+
+      const aNode = new RichText( GQSymbols.a, {
+        font: font,
+        fill: GQColors.STANDARD_FORM_A
+      } );
+
+      const bNode = new RichText( GQSymbols.b, {
+        font: font,
+        fill: GQColors.STANDARD_FORM_B
+      } );
+
+      const cNode = new RichText( GQSymbols.c, {
+        font: font,
+        fill: GQColors.STANDARD_FORM_C
+      } );
+
+      const iconNode = new HBox( {
+        spacing: 12,
+        children: [ aNode, bNode, cNode ]
+      } );
+
+      return new ScreenIcon( iconNode, {
+        fill: GQColors.SCREEN_BACKGROUND,
+        maxIconWidthProportion: 0.7
+      } );
     },
 
     /**
@@ -35,7 +94,33 @@ define( require => {
      * @returns {Node}
      */
     createVertexFormScreenIcon() {
-      return null;
+
+      const font = new PhetFont( { size: 40, weight: 'bold' } );
+
+      const aNode = new RichText( GQSymbols.a, {
+        font: font,
+        fill: GQColors.VERTEX_FORM_A
+      } );
+
+      const hNode = new RichText( GQSymbols.h, {
+        font: font,
+        fill: GQColors.VERTEX_FORM_H
+      } );
+
+      const kNode = new RichText( GQSymbols.k, {
+        font: font,
+        fill: GQColors.VERTEX_FORM_K
+      } );
+
+      const iconNode = new HBox( {
+        spacing: 8,
+        children: [ aNode, hNode, kNode ]
+      } );
+
+      return new ScreenIcon( iconNode, {
+        fill: GQColors.SCREEN_BACKGROUND,
+        maxIconWidthProportion: 0.7
+      } );
     },
 
     /**
@@ -43,7 +128,30 @@ define( require => {
      * @returns {Node}
      */
     createFocusAndDirectrixScreenIcon() {
-      return null;
+
+      const focusNode = new Manipulator( 12, GQColors.FOCUS, {
+        haloAlpha: 0,
+        pickable: false
+      } );
+
+      const directrixNode = new DirectrixNode(
+        new Property( new Quadratic( 1, 0, 0 ) ),
+        new Range( -50, 50 ),
+        new Range( -50, 50 ),
+        ModelViewTransform2.createIdentity(),
+        new BooleanProperty( true ),
+        new BooleanProperty( false ) );
+
+      const iconNode = new VBox( {
+        children: [ focusNode, directrixNode ],
+        spacing: 2
+      } );
+
+      return new ScreenIcon( iconNode, {
+        fill: GQColors.SCREEN_BACKGROUND,
+        maxIconWidthProportion: 0.7,
+        maxIconHeightProportion: 0.5
+      } );
     }
   };
 
