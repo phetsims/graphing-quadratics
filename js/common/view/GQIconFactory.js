aa2b9f496f0b441fc99e6e56ace0db33ad9c4e99
Chris Malley <cmalley@pixelzoom.com>
2018-10-04 14:41:30 -0600

    first pass at programmatic screen icons, needs polishing #11
    Signed-off-by: Chris Malley <cmalley@pixelzoom.com>

--- js/common/view/GQIconFactory.js
+++ js/common/view/GQIconFactory.js
@@ -1,6 +1,6 @@
 // Copyright 2018, University of Colorado Boulder
 
-//TODO #11 design screen icons
+//TODO #11 where possible, compute magic numbers herein for layout
 /**
  * Creates icons for this sim.
  *
@@ -10,21 +10,26 @@ define( require => {
   'use strict';
 
   // modules
+  const Circle = require( 'SCENERY/nodes/Circle' );
   const GQColors = require( 'GRAPHING_QUADRATICS/common/GQColors' );
-  const GQConstants = require( 'GRAPHING_QUADRATICS/common/GQConstants' );
-  const GQSymbols = require( 'GRAPHING_QUADRATICS/common/GQSymbols' );
   const graphingQuadratics = require( 'GRAPHING_QUADRATICS/graphingQuadratics' );
-  const HBox = require( 'SCENERY/nodes/HBox' );
   const Line = require( 'SCENERY/nodes/Line' );
   const Manipulator = require( 'GRAPHING_LINES/common/view/manipulator/Manipulator' );
+  const Node = require( 'SCENERY/nodes/Node' );
   const Path = require( 'SCENERY/nodes/Path' );
-  const PhetFont = require( 'SCENERY_PHET/PhetFont' );
   const Quadratic = require( 'GRAPHING_QUADRATICS/common/model/Quadratic' );
   const Range = require( 'DOT/Range' );
-  const RichText = require( 'SCENERY/nodes/RichText' );
   const ScreenIcon = require( 'JOIST/ScreenIcon' );
   const Shape = require( 'KITE/Shape' );
   const VBox = require( 'SCENERY/nodes/VBox' );
+  const VSeparator = require( 'SUN/VSeparator' );
+
+  // coefficients for the quadratic curve used in every icon
+  const A = 0.03;
+  const B = 0;
+  const C = 0;
+  const POINT_RADIUS = 12;
+  const MANIPULATOR_RADIUS = 12;
 
   const GQIconFactory = {
 
@@ -34,22 +39,24 @@ define( require => {
      */
     createExploreScreenIcon() {
 
-      const quadratic = new Quadratic( -0.05, 0, 0, {
-        color: GQColors.EXPLORE_INTERACTIVE_CURVE
-      } );
+      const separator = new VSeparator( 20, { stroke: 'transparent' } );
+
+      const quadratic = new Quadratic( A, B, C, { color: 'black' } );
 
       const bezierControlPoints = quadratic.getControlPoints( new Range( -50, 50 ) );
 
       const quadraticNode = new Path( new Shape()
         .moveToPoint( bezierControlPoints.startPoint )
         .quadraticCurveToPoint( bezierControlPoints.controlPoint, bezierControlPoints.endPoint ), {
-        lineWidth: 5,
+        lineWidth: 6,
         stroke: quadratic.color
       } );
 
-      return new ScreenIcon( quadraticNode, {
+      const iconNode = new VBox( { children: [ separator, quadraticNode ] } );
+
+      return new ScreenIcon( iconNode, {
         fill: GQColors.SCREEN_BACKGROUND,
-        maxIconWidthProportion: 0.7
+        maxIconHeightProportion: 1 // fill the full height of the screen icon
       } );
     },
 
@@ -59,31 +66,56 @@ define( require => {
      */
     createStandardFormScreenIcon() {
 
-      const font = new PhetFont( { size: 40, weight: 'bold' } );
+      const quadratic = new Quadratic( -A, B, C, { color: 'black' } );
 
-      const aNode = new RichText( GQSymbols.a, {
-        font: font,
-        fill: GQColors.STANDARD_FORM_A
+      const bezierControlPoints = quadratic.getControlPoints( new Range( -50, 50 ) );
+
+      const quadraticNode = new Path( new Shape()
+        .moveToPoint( bezierControlPoints.startPoint )
+        .quadraticCurveToPoint( bezierControlPoints.controlPoint, bezierControlPoints.endPoint ), {
+        lineWidth: 6,
+        stroke: quadratic.color
       } );
 
-      const bNode = new RichText( GQSymbols.b, {
-        font: font,
-        fill: GQColors.STANDARD_FORM_B
+      const xAxisNode = new Line( 0, 0, 140, 0, {
+        lineWidth: 1,
+        stroke: 'black',
+        centerX: quadraticNode.centerX,
+        centerY: quadraticNode.centerY
       } );
 
-      const cNode = new RichText( GQSymbols.c, {
-        font: font,
-        fill: GQColors.STANDARD_FORM_C
+      const vertexNode = new Circle( POINT_RADIUS, {
+        fill: GQColors.VERTEX,
+        centerX: quadraticNode.centerX,
+        centerY: quadraticNode.y
       } );
 
-      const iconNode = new HBox( {
-        spacing: 12,
-        children: [ aNode, bNode, cNode ]
+      const leftRootNode = new Circle( POINT_RADIUS, {
+        fill: GQColors.ROOTS,
+        centerX: quadraticNode.centerX - 35,
+        centerY: xAxisNode.y
+      } );
+
+      const rightRootNode = new Circle( POINT_RADIUS, {
+        fill: GQColors.ROOTS,
+        centerX: quadraticNode.centerX + 35,
+        centerY: xAxisNode.y
+      } );
+
+      const separator = new VSeparator( 15, {
+        stroke: 'transparent',
+        centerX: quadraticNode.centerX,
+        top: quadraticNode.bottom
+      } );
+
+      const iconNode = new Node( {
+        children: [ xAxisNode, quadraticNode, vertexNode, leftRootNode, rightRootNode, separator ]
       } );
 
       return new ScreenIcon( iconNode, {
         fill: GQColors.SCREEN_BACKGROUND,
-        maxIconWidthProportion: 0.7
+        maxIconWidthProportion: 1, // fill the full width of the screen icon
+        maxIconHeightProportion: 1 // fill the full height of the screen icon
       } );
     },
 
@@ -93,31 +125,33 @@ define( require => {
      */
     createVertexFormScreenIcon() {
 
-      const font = new PhetFont( { size: 40, weight: 'bold' } );
+      const separator = new VSeparator( 20, { stroke: 'transparent' } );
 
-      const aNode = new RichText( GQSymbols.a, {
-        font: font,
-        fill: GQColors.VERTEX_FORM_A
+      const quadratic = new Quadratic( A, B, C, { color: 'black' } );
+
+      const bezierControlPoints = quadratic.getControlPoints( new Range( -50, 50 ) );
+
+      const quadraticNode = new Path( new Shape()
+        .moveToPoint( bezierControlPoints.startPoint )
+        .quadraticCurveToPoint( bezierControlPoints.controlPoint, bezierControlPoints.endPoint ), {
+        lineWidth: 6,
+        stroke: quadratic.color,
+        centerX: separator.centerX,
+        top: separator.bottom
       } );
 
-      const hNode = new RichText( GQSymbols.h, {
-        font: font,
-        fill: GQColors.VERTEX_FORM_H
+      const vertexNode = new Manipulator( MANIPULATOR_RADIUS, GQColors.VERTEX, {
+        haloAlpha: 0,
+        pickable: false,
+        centerX: quadraticNode.centerX,
+        centerY: quadraticNode.y
       } );
 
-      const kNode = new RichText( GQSymbols.k, {
-        font: font,
-        fill: GQColors.VERTEX_FORM_K
-      } );
-
-      const iconNode = new HBox( {
-        spacing: 8,
-        children: [ aNode, hNode, kNode ]
-      } );
+      const iconNode = new Node( { children: [ separator, quadraticNode, vertexNode ] } );
 
       return new ScreenIcon( iconNode, {
         fill: GQColors.SCREEN_BACKGROUND,
-        maxIconWidthProportion: 0.7
+        maxIconHeightProportion: 1 // fill the full height of the screen icon
       } );
     },
 
@@ -127,26 +161,48 @@ define( require => {
      */
     createFocusAndDirectrixScreenIcon() {
 
-      const focusNode = new Manipulator( 12, GQColors.FOCUS, {
-        haloAlpha: 0,
-        pickable: false
+      const quadratic = new Quadratic( -A, B, C, { color: 'black' } );
+
+      const bezierControlPoints = quadratic.getControlPoints( new Range( -45, 45 ) );
+
+      const quadraticNode = new Path( new Shape()
+        .moveToPoint( bezierControlPoints.startPoint )
+        .quadraticCurveToPoint( bezierControlPoints.controlPoint, bezierControlPoints.endPoint ), {
+        lineWidth: 6,
+        stroke: quadratic.color
       } );
 
-      const directrixNode = new Line( 0, 0, 60, 0, {
+      const p = 35;
+      
+      const focusNode = new Manipulator( MANIPULATOR_RADIUS, GQColors.FOCUS, {
+        haloAlpha: 0,
+        pickable: false,
+        centerX: quadraticNode.centerX,
+        centerY: quadraticNode.y - p
+      } );
+
+      const directrixNode = new Line( 0, 0, 155, 0, {
         stroke: GQColors.DIRECTRIX,
         lineWidth: 4,
-        lineDash: GQConstants.DIRECTRIX_LINE_DASH
+        lineDash: [ 5, 5 ],
+        centerX: quadraticNode.centerX,
+        centerY: quadraticNode.y + p
       } );
 
-      const iconNode = new VBox( {
-        children: [ focusNode, directrixNode ],
-        spacing: 20
+      const separator = new VSeparator( 20, {
+        stroke: 'transparent',
+        centerX: directrixNode.centerX,
+        top: directrixNode.bottom
+      } );
+
+      const iconNode = new Node( {
+        children: [ quadraticNode, focusNode, directrixNode, separator ]
       } );
 
       return new ScreenIcon( iconNode, {
         fill: GQColors.SCREEN_BACKGROUND,
-        maxIconWidthProportion: 0.7,
-        maxIconHeightProportion: 0.5
+        maxIconWidthProportion: 1, // fill the full width of the screen icon
+        maxIconHeightProportion: 1 // fill the full height of the screen icon
       } );
     }
   };
