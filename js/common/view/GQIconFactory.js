da33a03b3cbf9b727ac5a4a51593d9272bc63485
Chris Malley <cmalley@pixelzoom.com>
2018-10-08 21:14:06 -0600

    refine screen icons #11
    Signed-off-by: Chris Malley <cmalley@pixelzoom.com>

--- js/common/view/GQIconFactory.js
+++ js/common/view/GQIconFactory.js
@@ -1,10 +1,8 @@
 // Copyright 2018, University of Colorado Boulder
 
-//TODO #11 where possible, compute magic numbers herein for layout
-//TODO #11 smooth out the ends of parabolas
-//TODO #11 fill the entire icon width and height
 /**
- * Creates icons for this sim.
+ * Creates screen icons for this sim.  Most of the magic numbers herein were determined empirically,
+ * to match the mockups provided in https://github.com/phetsims/graphing-quadratics/issues/11#issuecomment-427149327.
  *
  * @author Chris Malley (PixelZoom, Inc.)
  */
@@ -21,17 +19,28 @@ define( require => {
   const Path = require( 'SCENERY/nodes/Path' );
   const Quadratic = require( 'GRAPHING_QUADRATICS/common/model/Quadratic' );
   const Range = require( 'DOT/Range' );
+  const Rectangle = require( 'SCENERY/nodes/Rectangle' );
+  const Screen = require( 'JOIST/Screen' );
   const ScreenIcon = require( 'JOIST/ScreenIcon' );
   const Shape = require( 'KITE/Shape' );
-  const VBox = require( 'SCENERY/nodes/VBox' );
-  const VSeparator = require( 'SUN/VSeparator' );
+
+  // constants
+  const ICON_SIZE = Screen.MINIMUM_HOME_SCREEN_ICON_SIZE; // so we can draw to the edges of the icons
+  const CLIP_AREA = Shape.rect( 0, 0, ICON_SIZE.width, ICON_SIZE.height );
+  const LINE_WIDTH = 20;
+  const POINT_RADIUS = 35;
+  const VERTEX_MANIPULATOR_RADIUS = 40;
+  const FOCUS_MANIPULATOR_RADIUS = 25;
+  const SCREEN_ICON_OPTIONS = {
+    fill: GQColors.SCREEN_BACKGROUND,
+    maxIconWidthProportion: 1,
+    maxIconHeightProportion: 1
+  };
 
   // coefficients for the quadratic curve used in every icon
-  const A = 0.03;
+  const A = 0.0075;
   const B = 0;
   const C = 0;
-  const POINT_RADIUS = 10;
-  const MANIPULATOR_RADIUS = 12;
 
   const GQIconFactory = {
 
@@ -41,25 +50,28 @@ define( require => {
      */
     createExploreScreenIcon() {
 
-      const separator = new VSeparator( 20, { stroke: 'transparent' } );
+      // invisible rectangle that fills the entire icon
+      const rectangle = new Rectangle( 0, 0, ICON_SIZE.width, ICON_SIZE.height );
 
+      // parabola that opens downward
       const quadratic = new Quadratic( A, B, C, { color: 'black' } );
-
-      const bezierControlPoints = quadratic.getControlPoints( new Range( -50, 50 ) );
-
-      const quadraticNode = new Path( new Shape()
+      const bezierControlPoints = quadratic.getControlPoints( new Range( -250, 250 ) );
+      const parabolaNode = new Path( new Shape()
         .moveToPoint( bezierControlPoints.startPoint )
         .quadraticCurveToPoint( bezierControlPoints.controlPoint, bezierControlPoints.endPoint ), {
-        lineWidth: 6,
-        stroke: quadratic.color
+        lineWidth: LINE_WIDTH,
+        stroke: quadratic.color,
+        centerX: rectangle.centerX,
+        top: rectangle.top + ( 0.2 * rectangle.height )
       } );
 
-      const iconNode = new VBox( { children: [ separator, quadraticNode ] } );
-
-      return new ScreenIcon( iconNode, {
-        fill: GQColors.SCREEN_BACKGROUND,
-        maxIconHeightProportion: 1 // fill the full height of the screen icon
+      // put all of the pieces together, clipped to the icon size
+      const iconNode = new Node( {
+        children: [ rectangle, parabolaNode ],
+        clipArea: CLIP_AREA
       } );
+
+      return new ScreenIcon( iconNode, SCREEN_ICON_OPTIONS );
     },
 
     /**
@@ -68,57 +80,58 @@ define( require => {
      */
     createStandardFormScreenIcon() {
 
+      // invisible rectangle that fills the entire icon
+      const rectangle = new Rectangle( 0, 0, ICON_SIZE.width, ICON_SIZE.height );
+
+      // parabola that opens upward
       const quadratic = new Quadratic( -A, B, C, { color: 'black' } );
-
-      const bezierControlPoints = quadratic.getControlPoints( new Range( -50, 50 ) );
-
-      const quadraticNode = new Path( new Shape()
+      const bezierControlPoints = quadratic.getControlPoints( new Range( -250, 250 ) );
+      const parabolaNode = new Path( new Shape()
         .moveToPoint( bezierControlPoints.startPoint )
         .quadraticCurveToPoint( bezierControlPoints.controlPoint, bezierControlPoints.endPoint ), {
-        lineWidth: 6,
-        stroke: quadratic.color
+        lineWidth: LINE_WIDTH,
+        stroke: quadratic.color,
+        centerX: rectangle.centerX,
+        y: 0.85 * rectangle.height
       } );
 
-      const xAxisNode = new Line( 0, 0, 140, 0, {
-        lineWidth: 1,
+      // x axis, fills the width of the icon
+      const xAxisNode = new Line( 0, 0, ICON_SIZE.width, 0, {
+        lineWidth: 5,
         stroke: 'black',
-        centerX: quadraticNode.centerX,
-        centerY: quadraticNode.centerY
+        centerX: parabolaNode.centerX,
+        centerY: 0.4 * rectangle.height
       } );
 
+      // vertex
       const vertexNode = new Circle( POINT_RADIUS, {
         fill: GQColors.VERTEX,
-        centerX: quadraticNode.centerX,
-        centerY: quadraticNode.y
+        centerX: parabolaNode.centerX,
+        centerY: parabolaNode.y
       } );
 
+      // eyeballed - horizontal distance from vertex.x
+      const root = 152;
+
+      // roots
       const leftRootNode = new Circle( POINT_RADIUS, {
         fill: GQColors.ROOTS,
-        centerX: quadraticNode.centerX - 35,
+        centerX: parabolaNode.centerX - root,
         centerY: xAxisNode.y
       } );
-
       const rightRootNode = new Circle( POINT_RADIUS, {
         fill: GQColors.ROOTS,
-        centerX: quadraticNode.centerX + 35,
+        centerX: parabolaNode.centerX + root,
         centerY: xAxisNode.y
       } );
 
-      const separator = new VSeparator( 15, {
-        stroke: 'transparent',
-        centerX: quadraticNode.centerX,
-        top: quadraticNode.bottom
-      } );
-
+      // put all of the pieces together, clipped to the icon size
       const iconNode = new Node( {
-        children: [ xAxisNode, quadraticNode, vertexNode, leftRootNode, rightRootNode, separator ]
+        children: [ rectangle, xAxisNode, parabolaNode, vertexNode, leftRootNode, rightRootNode ],
+        clipArea: CLIP_AREA
       } );
 
-      return new ScreenIcon( iconNode, {
-        fill: GQColors.SCREEN_BACKGROUND,
-        maxIconWidthProportion: 1, // fill the full width of the screen icon
-        maxIconHeightProportion: 1 // fill the full height of the screen icon
-      } );
+      return new ScreenIcon( iconNode, SCREEN_ICON_OPTIONS );
     },
 
     /**
@@ -127,34 +140,36 @@ define( require => {
      */
     createVertexFormScreenIcon() {
 
-      const separator = new VSeparator( 20, { stroke: 'transparent' } );
+      // invisible rectangle that fills the entire icon
+      const rectangle = new Rectangle( 0, 0, ICON_SIZE.width, ICON_SIZE.height );
 
+      // parabola that opens downward
       const quadratic = new Quadratic( A, B, C, { color: 'black' } );
-
-      const bezierControlPoints = quadratic.getControlPoints( new Range( -50, 50 ) );
-
-      const quadraticNode = new Path( new Shape()
+      const bezierControlPoints = quadratic.getControlPoints( new Range( -250, 250 ) );
+      const parabolaNode = new Path( new Shape()
         .moveToPoint( bezierControlPoints.startPoint )
         .quadraticCurveToPoint( bezierControlPoints.controlPoint, bezierControlPoints.endPoint ), {
-        lineWidth: 6,
+        lineWidth: LINE_WIDTH,
         stroke: quadratic.color,
-        centerX: separator.centerX,
-        top: separator.bottom
+        centerX: rectangle.centerX,
+        top: rectangle.top + ( 0.2 * ICON_SIZE.height )
       } );
 
-      const vertexNode = new Manipulator( MANIPULATOR_RADIUS, GQColors.VERTEX, {
+      // vertex
+      const vertexNode = new Manipulator( VERTEX_MANIPULATOR_RADIUS, GQColors.VERTEX, {
         haloAlpha: 0,
         pickable: false,
-        centerX: quadraticNode.centerX,
-        centerY: quadraticNode.y
+        centerX: parabolaNode.centerX,
+        centerY: parabolaNode.y
       } );
 
-      const iconNode = new Node( { children: [ separator, quadraticNode, vertexNode ] } );
-
-      return new ScreenIcon( iconNode, {
-        fill: GQColors.SCREEN_BACKGROUND,
-        maxIconHeightProportion: 1 // fill the full height of the screen icon
+      // put all of the pieces together, clipped to the icon size
+      const iconNode = new Node( {
+        children: [ rectangle, parabolaNode, vertexNode ],
+        clipArea: CLIP_AREA
       } );
+
+      return new ScreenIcon( iconNode, SCREEN_ICON_OPTIONS );
     },
 
     /**
@@ -163,49 +178,48 @@ define( require => {
      */
     createFocusAndDirectrixScreenIcon() {
 
+      // invisible rectangle that fills the entire icon
+      const rectangle = new Rectangle( 0, 0, ICON_SIZE.width, ICON_SIZE.height );
+
+      // parabola that opens upward
       const quadratic = new Quadratic( -A, B, C, { color: 'black' } );
-
-      const bezierControlPoints = quadratic.getControlPoints( new Range( -45, 45 ) );
-
-      const quadraticNode = new Path( new Shape()
+      const bezierControlPoints = quadratic.getControlPoints( new Range( -250, 250 ) );
+      const parabolaNode = new Path( new Shape()
         .moveToPoint( bezierControlPoints.startPoint )
         .quadraticCurveToPoint( bezierControlPoints.controlPoint, bezierControlPoints.endPoint ), {
-        lineWidth: 6,
-        stroke: quadratic.color
+        lineWidth: LINE_WIDTH,
+        stroke: quadratic.color,
+        centerX: rectangle.centerX,
+        y: 0.55 * ICON_SIZE.height
       } );
 
-      const p = 35;
-      
-      const focusNode = new Manipulator( MANIPULATOR_RADIUS, GQColors.FOCUS, {
+      // eyeballed - vertical distance between focus and vertex, focus and directrix
+      const p = 100;
+
+      // focus above the parabola's vertex
+      const focusNode = new Manipulator( FOCUS_MANIPULATOR_RADIUS, GQColors.FOCUS, {
         haloAlpha: 0,
         pickable: false,
-        centerX: quadraticNode.centerX,
-        centerY: quadraticNode.y - p
+        centerX: parabolaNode.centerX,
+        centerY: parabolaNode.y - p
       } );
 
-      const directrixNode = new Line( 0, 0, 155, 0, {
+      // directrix below the parabola's vertex, fills the width of the icon
+      const directrixNode = new Line( 0, 0, ICON_SIZE.width, 0, {
         stroke: GQColors.DIRECTRIX,
-        lineWidth: 4,
-        lineDash: [ 5, 5 ],
-        centerX: quadraticNode.centerX,
-        centerY: quadraticNode.y + p
+        lineWidth: 15,
+        lineDash: [ 24, 24 ],
+        centerX: parabolaNode.centerX,
+        centerY: parabolaNode.y + p
       } );
 
-      const separator = new VSeparator( 20, {
-        stroke: 'transparent',
-        centerX: directrixNode.centerX,
-        top: directrixNode.bottom
-      } );
-
+      // put all of the pieces together, clipped to the icon size
       const iconNode = new Node( {
-        children: [ quadraticNode, focusNode, directrixNode, separator ]
+        children: [ rectangle, parabolaNode, focusNode, directrixNode ],
+        clipArea: CLIP_AREA
       } );
 
-      return new ScreenIcon( iconNode, {
-        fill: GQColors.SCREEN_BACKGROUND,
-        maxIconWidthProportion: 1, // fill the full width of the screen icon
-        maxIconHeightProportion: 1 // fill the full height of the screen icon
-      } );
+      return new ScreenIcon( iconNode, SCREEN_ICON_OPTIONS );
     }
   };
 
