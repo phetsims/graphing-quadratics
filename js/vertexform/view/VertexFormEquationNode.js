60688eb2da865c93214bb0647f33a09e3aaed511
Chris Malley <cmalley@pixelzoom.com>
2018-11-12 11:43:49 -0700

    wrap VertexFormEquationNode, like StandardFormEquationNode
    Signed-off-by: Chris Malley <cmalley@pixelzoom.com>

--- js/vertexform/view/VertexFormEquationNode.js
+++ js/vertexform/view/VertexFormEquationNode.js
@@ -14,11 +14,12 @@ define( require => {
   const GQSymbols = require( 'GRAPHING_QUADRATICS/common/GQSymbols' );
   const graphingQuadratics = require( 'GRAPHING_QUADRATICS/graphingQuadratics' );
   const MathSymbols = require( 'SCENERY_PHET/MathSymbols' );
+  const Node = require( 'SCENERY/nodes/Node' );
   const RichText = require( 'SCENERY/nodes/RichText' );
   const StringUtils = require( 'PHETCOMMON/util/StringUtils' );
   const Tandem = require( 'TANDEM/Tandem' );
 
-  class VertexFormEquationNode extends RichText {
+  class VertexFormEquationNode extends Node {
 
     /**
      * @param {Object} [options]
@@ -26,15 +27,11 @@ define( require => {
     constructor( options ) {
 
       options = _.extend( {
-
-        // RichText options
-        font: GQConstants.INTERACTIVE_EQUATION_FONT,
-        fill: 'black',
         tandem: Tandem.required
       }, options );
 
       // y = a(x - h)^2 + k
-      const equation = StringUtils.fillIn( '{{y}} {{equals}} {{a}}({{x}} {{minus}} {{h}})<sup>2</sup> {{plus}} {{k}}', {
+      const text = StringUtils.fillIn( '{{y}} {{equals}} {{a}}({{x}} {{minus}} {{h}})<sup>2</sup> {{plus}} {{k}}', {
         x: GQSymbols.x,
         y: GQSymbols.y,
         a: GQSymbols.a,
@@ -45,7 +42,16 @@ define( require => {
         plus: MathSymbols.PLUS
       } );
 
-      super( equation, options );
+      const textNode = new RichText( text, {
+        font: GQConstants.INTERACTIVE_EQUATION_FONT,
+        fill: 'black'
+      } );
+
+      // Wrap the RichText so that its API is not accessible to clients or PhET-iO.
+      assert && assert( !options.children, 'VertexFormEquationNode sets children' );
+      options.children = [ textNode ];
+
+      super( options );
     }
   }
 
