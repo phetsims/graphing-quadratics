f040513812a69ffcfdcb08aaf3f12d6712173503
Chris Malley <cmalley@pixelzoom.com>
2018-11-29 15:09:13 -0700

    move SaveEraseButtonGroup to an inner class of GQEquationAccordionBox
    Signed-off-by: Chris Malley <cmalley@pixelzoom.com>

--- js/common/view/GQEquationAccordionBox.js
+++ js/common/view/GQEquationAccordionBox.js
@@ -10,18 +10,25 @@ define( require => {
 
   // modules
   const AccordionBox = require( 'SUN/AccordionBox' );
+  const EraserButton = require( 'SCENERY_PHET/buttons/EraserButton' );
+  const FontAwesomeNode = require( 'SUN/FontAwesomeNode' );
   const GQColors = require( 'GRAPHING_QUADRATICS/common/GQColors' );
   const GQConstants = require( 'GRAPHING_QUADRATICS/common/GQConstants' );
   const graphingQuadratics = require( 'GRAPHING_QUADRATICS/graphingQuadratics' );
+  const HBox = require( 'SCENERY/nodes/HBox' );
   const HSeparator = require( 'SUN/HSeparator' );
-  const SaveEraseButtonGroup = require( 'GRAPHING_QUADRATICS/common/view/SaveEraseButtonGroup' );
+  const PhetColorScheme = require( 'SCENERY_PHET/PhetColorScheme' );
+  const RectangularPushButton = require( 'SUN/buttons/RectangularPushButton' );
   const Tandem = require( 'TANDEM/Tandem' );
   const VBox = require( 'SCENERY/nodes/VBox' );
+
+  // constants
+  const BUTTON_ICON_WIDTH = 30;
 
   class GQEquationAccordionBox extends AccordionBox {
 
     /**
-     * @param {ExploreModel} model
+     * @param {GQModel} model
      * @param {Node} interactiveEquationNode
      * @param {Object} [options]
      * @abstract
@@ -37,12 +44,10 @@ define( require => {
 
       }, GQConstants.ACCORDION_BOX_OPTIONS, options );
 
-      const buttonGroup = new SaveEraseButtonGroup(
-        model.saveQuadratic.bind( model ),
-        model.eraseQuadratic.bind( model ),
-        model.savedQuadraticProperty, {
-          tandem: options.tandem.createTandem( 'buttonGroup' )
-        } );
+      // Buttons at the bottom of the accordion box
+      const buttonGroup = new ButtonGroup( model, {
+        tandem: options.tandem.createTandem( 'buttonGroup' )
+      } );
 
       // properties of the horizontal separators
       const separatorWidth = Math.max( interactiveEquationNode.width, buttonGroup.width );
@@ -63,5 +68,64 @@ define( require => {
     }
   }
 
-  return graphingQuadratics.register( 'GQEquationAccordionBox', GQEquationAccordionBox );
+  graphingQuadratics.register( 'GQEquationAccordionBox', GQEquationAccordionBox );
+
+  /**
+   * The buttons at the bottom of the accordion box.
+   */
+  class ButtonGroup extends HBox {
+
+    /**
+     * @param {GQModel} model
+     * @param {Object} [options]
+     */
+    constructor( model, options ) {
+
+      options = _.extend( {
+
+        // HBox options
+        spacing: 40,
+        align: 'center',
+
+        // phet-io
+        tandem: Tandem.required,
+        phetioDocumentation: 'buttons that appear below the interactive equation',
+        phetioComponentOptions: { visibleProperty: { phetioFeatured: true } }
+
+      }, options );
+
+      // Save button
+      const saveButton = new RectangularPushButton( {
+        content: new FontAwesomeNode( 'camera', { maxWidth: BUTTON_ICON_WIDTH } ),
+        baseColor: PhetColorScheme.BUTTON_YELLOW,
+        listener: () => { model.saveQuadratic(); },
+        tandem: options.tandem.createTandem( 'saveButton' ),
+        phetioDocumentation: 'the button used to save a quadratic',
+        phetioReadOnly: true // we don't want the client to modify this button, see #60
+      } );
+
+      // Erase button
+      const eraseButton = new EraserButton( {
+        iconWidth: BUTTON_ICON_WIDTH,
+        listener: () => { model.eraseQuadratic(); },
+        tandem: options.tandem.createTandem( 'eraseButton' ),
+        phetioDocumentation: 'the button used to erase the saved quadratic',
+        phetioReadOnly: true
+      } );
+
+      assert && assert( !options.children, 'ButtonGroup sets children' );
+      options.children = [ saveButton, eraseButton ];
+
+      super( options );
+
+      // Enable the erase button when there is a saved quadratic
+      model.savedQuadraticProperty.link( savedQuadratic => {
+        eraseButton.enabled = ( savedQuadratic !== null );
+      } );
+    }
+  }
+
+  graphingQuadratics.register( 'ButtonGroup', ButtonGroup );
+
+  return GQEquationAccordionBox;
 } );
