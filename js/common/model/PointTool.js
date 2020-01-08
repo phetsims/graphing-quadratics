1acc93ed719e501e3a096b87d7854856a1f1a2f9
phet-dev <phetadmin@gmail.com>
2020-01-08 03:17:05 -0700

    update copyright dates from daily grunt work

--- js/common/model/PointTool.js
+++ js/common/model/PointTool.js
@@ -1,4 +1,4 @@
-// Copyright 2018-2019, University of Colorado Boulder
+// Copyright 2018-2020, University of Colorado Boulder
 
 /**
  * Model of the point tool. Knows when it is placed on one of the quadratics.
