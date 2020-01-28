49688e481b7d29168d811cc62ae6afe14728840d
zepumph <michael.kauzmann@colorado.edu>
2020-01-27 16:58:02 -0900

    add phetioDocumentation, https://github.com/phetsims/joist/issues/591

--- js/phet-io/graphing-quadratics-phet-io-elements-baseline.js
+++ js/phet-io/graphing-quadratics-phet-io-elements-baseline.js
@@ -8686,7 +8686,7 @@ window.phet.phetio.phetioElementsBaseline = assert &&
       "phetioTypeName": "PropertyIO<BooleanIO>"
     },
     "graphingQuadratics.general.soundManager.enabledProperty": {
-      "phetioDocumentation": "",
+      "phetioDocumentation": "If this sim contains sound, then this Property will toggle whether sound is enabled ordisabled.",
       "phetioDynamicElement": false,
       "phetioEventType": "MODEL",
       "phetioFeatured": false,
