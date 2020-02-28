93742a8758c5a695e52117baf17a1552ccf30411
zepumph <michael.kauzmann@colorado.edu>
2020-02-28 13:35:36 -0900

    update documentation, https://github.com/phetsims/studio/issues/103

--- js/phet-io/graphing-quadratics-phet-io-elements-baseline.js
+++ js/phet-io/graphing-quadratics-phet-io-elements-baseline.js
@@ -8713,7 +8713,7 @@ window.phet.phetio.phetioElementsBaseline = assert &&
       "phetioTypeName": "PropertyIO<BooleanIO>"
     },
     "graphingQuadratics.general.soundManager.enhancedSoundEnabledProperty": {
-      "phetioDocumentation": "If the sim contains sound, then this Property will toggle whether \"enhanced\" sound is enabled or disabled.",
+      "phetioDocumentation": "If the sim contains sound, then this Property will toggle whether \"enhanced\" sound is enabled or disabled. Note that not all simulations that support sound also support enhanced sound.",
       "phetioDynamicElement": false,
       "phetioEventType": "MODEL",
       "phetioFeatured": false,
