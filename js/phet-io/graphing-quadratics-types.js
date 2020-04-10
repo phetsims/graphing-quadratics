29ad87fae97ae2a1da2d88199239af680e1e4256
zepumph <michael.kauzmann@colorado.edu>
2020-04-10 12:48:16 -0800

    add parameterTypes to ArrayIO and ReferenceIO, assert parametric IO Types have that field, https://github.com/phetsims/tandem/issues/159

--- js/phet-io/graphing-quadratics-types.js
+++ js/phet-io/graphing-quadratics-types.js
@@ -219,6 +219,9 @@ window.phet.preloads.phetio.phetioTypes = assert &&
       "events": [],
       "methodOrder": [],
       "methods": {},
+      "parameterTypes": [
+        "StringIO"
+      ],
       "supertype": "ObjectIO",
       "typeName": "ArrayIO<StringIO>"
     },
@@ -1653,6 +1656,9 @@ window.phet.preloads.phetio.phetioTypes = assert &&
       "events": [],
       "methodOrder": [],
       "methods": {},
+      "parameterTypes": [
+        "ObjectIO"
+      ],
       "supertype": "ObjectIO",
       "typeName": "ReferenceIO<ObjectIO>"
     },
@@ -1677,6 +1683,9 @@ window.phet.preloads.phetio.phetioTypes = assert &&
       "events": [],
       "methodOrder": [],
       "methods": {},
+      "parameterTypes": [
+        "ObjectIO"
+      ],
       "supertype": "ReferenceIO<ObjectIO>",
       "typeName": "ScreenIO"
     },
