7f21efdd82d9644b27e1f0026b2ab075fc02ad70
samreid <reids@colorado.edu>
2020-09-17 20:59:03 -0600

    Convert to IOType, see https://github.com/phetsims/tandem/issues/211

--- js/common/model/QuadraticIO.js
+++ js/common/model/QuadraticIO.js
@@ -6,41 +6,16 @@
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
-import validate from '../../../../axon/js/validate.js';
-import ObjectIO from '../../../../tandem/js/types/ObjectIO.js';
+import IOType from '../../../../tandem/js/types/IOType.js';
 import graphingQuadratics from '../../graphingQuadratics.js';
 import Quadratic from './Quadratic.js';
 
-class QuadraticIO extends ObjectIO {
-
-  /**
-   * Encodes the state of a Quadratic instance.
-   * @param {Quadratic} quadratic
-   * @returns {*}
-   * @public
-   * @override
-   */
-  static toStateObject( quadratic ) {
-    validate( quadratic, this.validator );
-    return quadratic.toStateObject();
-  }
-
-  /**
-   * Decodes state into a Quadratic instance.
-   * @param {*} stateObject
-   * @returns {Quadratic}
-   * @public
-   * @override
-   */
-  static fromStateObject( stateObject ) {
-    return Quadratic.fromStateObject( stateObject );
-  }
-}
-
-QuadraticIO.documentation = Quadratic.documentationQuadraticIO;
-QuadraticIO.validator = { valueType: Quadratic };
-QuadraticIO.typeName = 'QuadraticIO';
-ObjectIO.validateIOType( QuadraticIO );
+const QuadraticIO = new IOType( 'QuadraticIO', {
+  valueType: Quadratic,
+  documentation: Quadratic.documentationQuadraticIO,
+  toStateObject: quadratic => quadratic.toStateObject(),
+  fromStateObject: Quadratic.fromStateObject
+} );
 
 graphingQuadratics.register( 'QuadraticIO', QuadraticIO );
 export default QuadraticIO;
