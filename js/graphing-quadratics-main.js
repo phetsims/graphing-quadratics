5e01ea4a0ed305c46755be3a0700ada4f2ba34da
pixelzoom <cmalley@pixelzoom.com>
2021-07-07 09:46:03 -0600

    phetioDesigned:true, https://github.com/phetsims/graphing-quadratics/issues/152

--- js/graphing-quadratics-main.js
+++ js/graphing-quadratics-main.js
@@ -16,22 +16,27 @@ import graphingQuadraticsStrings from './graphingQuadraticsStrings.js';
 import StandardFormScreen from './standardform/StandardFormScreen.js';
 import VertexFormScreen from './vertexform/VertexFormScreen.js';
 
-const options = {
-  credits: {
-    leadDesign: 'Amanda McGarry',
-    softwareDevelopment: 'Chris Malley (PixelZoom, Inc.), Andrea Lin',
-    team: 'Mike Dubson, Karina K. R. Hensberry, Trish Loeblein, Ariel Paul, Kathy Perkins',
-    qualityAssurance: 'Jaspe Arias, Steele Dalton, Laura Rea, Jacob Romero, Ethan Ward, Kathryn Woessner, Kelly Wurtz'
-  }
-};
-
 simLauncher.launch( () => {
+
   const screens = [
     new ExploreScreen( Tandem.ROOT.createTandem( 'exploreScreen' ) ),
     new StandardFormScreen( Tandem.ROOT.createTandem( 'standardFormScreen' ) ),
     new VertexFormScreen( Tandem.ROOT.createTandem( 'vertexFormScreen' ) ),
     new FocusAndDirectrixScreen( Tandem.ROOT.createTandem( 'focusAndDirectrixScreen' ) )
   ];
+
+  const options = {
+    credits: {
+      leadDesign: 'Amanda McGarry',
+      softwareDevelopment: 'Chris Malley (PixelZoom, Inc.), Andrea Lin',
+      team: 'Mike Dubson, Karina K. R. Hensberry, Trish Loeblein, Ariel Paul, Kathy Perkins',
+      qualityAssurance: 'Jaspe Arias, Steele Dalton, Laura Rea, Jacob Romero, Ethan Ward, Kathryn Woessner, Kelly Wurtz'
+    },
+
+    // phet-io options
+    phetioDesigned: true
+  };
+
   const sim = new Sim( graphingQuadraticsStrings[ 'graphing-quadratics' ].title, screens, options );
   sim.start();
 } );
