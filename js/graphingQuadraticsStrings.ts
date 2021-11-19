0fa7e40366651c8748b0bda50dc836b22a7a2b17
Sam Reid <reids@colorado.edu>
2021-11-18 17:43:40 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

---
+++ js/graphingQuadraticsStrings.ts
@@ -0,0 +1,45 @@
+// Copyright 2020-2021, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import getStringModule from '../../chipper/js/getStringModule.js';
+import graphingQuadratics from './graphingQuadratics.js';
+
+type StringsType = {
+  'graphing-quadratics': {
+    'title': string
+  },
+  'screen': {
+    'explore': string,
+    'standardForm': string,
+    'vertexForm': string,
+    'focusAndDirectrix': string
+  },
+  'vertex': string,
+  'axisOfSymmetry': string,
+  'roots': string,
+  'coordinates': string,
+  'equations': string,
+  'noRealRoots': string,
+  'pointOnParabola': string,
+  'quadraticTerms': string,
+  'directrix': string,
+  'focus': string,
+  'x': string,
+  'y': string,
+  'a': string,
+  'b': string,
+  'c': string,
+  'h': string,
+  'k': string,
+  'p': string,
+  'coordinateUnknown': string
+};
+
+const graphingQuadraticsStrings = getStringModule( 'GRAPHING_QUADRATICS' ) as StringsType;
+
+graphingQuadratics.register( 'graphingQuadraticsStrings', graphingQuadraticsStrings );
+
+export default graphingQuadraticsStrings;
