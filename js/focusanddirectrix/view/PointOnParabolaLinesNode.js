c1d93f3390ede363d3576bcce9f275157b73e369
phet-dev <phetadmin@gmail.com>
2022-05-24 03:39:47 -0600

    update copyright dates from daily grunt work

--- js/focusanddirectrix/view/PointOnParabolaLinesNode.js
+++ js/focusanddirectrix/view/PointOnParabolaLinesNode.js
@@ -1,4 +1,4 @@
-// Copyright 2018-2021, University of Colorado Boulder
+// Copyright 2018-2022, University of Colorado Boulder
 
 /**
  * Dashed lines that connect the 'point on parabola' to the focus and directrix.
