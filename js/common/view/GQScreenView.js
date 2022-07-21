fe975facdd7498ce78087bd5fb677b6f9bc963c3
phet-dev <phetadmin@gmail.com>
2022-07-21 05:01:08 -0600

    update copyright dates from daily grunt work

--- js/common/view/GQScreenView.js
+++ js/common/view/GQScreenView.js
@@ -1,4 +1,4 @@
-// Copyright 2018-2021, University of Colorado Boulder
+// Copyright 2018-2022, University of Colorado Boulder
 
 /**
  * Base class for ScreenViews in this sim.
--- js/vertexform/view/VertexFormEquationNode.js
+++ js/vertexform/view/VertexFormEquationNode.js
@@ -1,4 +1,4 @@
-// Copyright 2018-2021, University of Colorado Boulder
+// Copyright 2018-2022, University of Colorado Boulder
 
 /**
  * Static equation in vertex form: y = a(x - h)^2 + k
