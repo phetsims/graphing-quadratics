0f28bf5e86e2098459b46fb9fa0aa2248ae69e57
phet-dev <phetadmin@gmail.com>
2022-08-11 04:39:01 -0600

    update copyright dates from daily grunt work

--- js/standardform/view/RootsNode.js
+++ js/standardform/view/RootsNode.js
@@ -1,4 +1,4 @@
-// Copyright 2018-2021, University of Colorado Boulder
+// Copyright 2018-2022, University of Colorado Boulder
 
 /**
  * Displays the roots of a quadratic as non-interactive points with coordinate labels.
