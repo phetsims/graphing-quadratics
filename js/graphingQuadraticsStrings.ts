c12e1f4e09ea03fdbcad5e20baadb4015f5f3c40
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-15 20:36:36 -0600

    Grunt update, and changes for string Properties, see https://github.com/phetsims/chipper/issues/1302

--- js/graphingQuadraticsStrings.ts
+++ js/graphingQuadraticsStrings.ts
@@ -5,37 +5,62 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
+import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
 import graphingQuadratics from './graphingQuadratics.js';
 
 type StringsType = {
   'graphing-quadratics': {
     'title': string;
+    'titleProperty': TReadOnlyProperty<string>;
   };
   'screen': {
     'explore': string;
+    'exploreProperty': TReadOnlyProperty<string>;
     'standardForm': string;
+    'standardFormProperty': TReadOnlyProperty<string>;
     'vertexForm': string;
+    'vertexFormProperty': TReadOnlyProperty<string>;
     'focusAndDirectrix': string;
+    'focusAndDirectrixProperty': TReadOnlyProperty<string>;
   };
   'vertex': string;
+  'vertexProperty': TReadOnlyProperty<string>;
   'axisOfSymmetry': string;
+  'axisOfSymmetryProperty': TReadOnlyProperty<string>;
   'roots': string;
+  'rootsProperty': TReadOnlyProperty<string>;
   'coordinates': string;
+  'coordinatesProperty': TReadOnlyProperty<string>;
   'equations': string;
+  'equationsProperty': TReadOnlyProperty<string>;
   'noRealRoots': string;
+  'noRealRootsProperty': TReadOnlyProperty<string>;
   'pointOnParabola': string;
+  'pointOnParabolaProperty': TReadOnlyProperty<string>;
   'quadraticTerms': string;
+  'quadraticTermsProperty': TReadOnlyProperty<string>;
   'directrix': string;
+  'directrixProperty': TReadOnlyProperty<string>;
   'focus': string;
+  'focusProperty': TReadOnlyProperty<string>;
   'x': string;
+  'xProperty': TReadOnlyProperty<string>;
   'y': string;
+  'yProperty': TReadOnlyProperty<string>;
   'a': string;
+  'aProperty': TReadOnlyProperty<string>;
   'b': string;
+  'bProperty': TReadOnlyProperty<string>;
   'c': string;
+  'cProperty': TReadOnlyProperty<string>;
   'h': string;
+  'hProperty': TReadOnlyProperty<string>;
   'k': string;
+  'kProperty': TReadOnlyProperty<string>;
   'p': string;
+  'pProperty': TReadOnlyProperty<string>;
   'coordinateUnknown': string;
+  'coordinateUnknownProperty': TReadOnlyProperty<string>;
 };
 
 const graphingQuadraticsStrings = getStringModule( 'GRAPHING_QUADRATICS' ) as StringsType;
