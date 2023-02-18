1ddf079309df6ccefe6c4d8f073f234cc31bb074
pixelzoom <cmalley@pixelzoom.com>
2023-02-18 13:42:09 -0700

    // @ts-nocheck

--- js/common/model/PointTool.js
+++ js/common/model/PointTool.js
@@ -1,5 +1,6 @@
 // Copyright 2018-2022, University of Colorado Boulder
 
+// @ts-nocheck
 /**
  * Model of the point tool. Knows when it is placed on one of the quadratics.
  *
--- js/common/model/Quadratic.js
+++ js/common/model/Quadratic.js
@@ -1,5 +1,6 @@
 // Copyright 2018-2022, University of Colorado Boulder
 
+// @ts-nocheck
 /**
  * An immutable quadratic, described by these equations:
  *
