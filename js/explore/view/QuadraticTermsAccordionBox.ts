d8c4a6b5f0244ffb6cb8cd8a668f628cb88eef41
pixelzoom <cmalley@pixelzoom.com>
2023-02-20 15:21:17 -0700

    convert to TypeScript

--- js/explore/view/QuadraticTermsAccordionBox.ts
+++ js/explore/view/QuadraticTermsAccordionBox.ts
@@ -1,6 +1,5 @@
 // Copyright 2018-2022, University of Colorado Boulder
 
-// @ts-nocheck
 /**
  * Accordion box for showing and hiding terms of the interactive quadratic equation.
  *
@@ -8,38 +7,35 @@
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
-import merge from '../../../../phet-core/js/merge.js';
-import { HSeparator, Text, VBox } from '../../../../scenery/js/imports.js';
-import AccordionBox from '../../../../sun/js/AccordionBox.js';
-import Tandem from '../../../../tandem/js/Tandem.js';
+import { EmptySelfOptions, optionize4 } from '../../../../phet-core/js/optionize.js';
+import PickRequired from '../../../../phet-core/js/types/PickRequired.js';
+import { AlignBox, AlignBoxOptions, AlignGroup, HSeparator, Text, VBox } from '../../../../scenery/js/imports.js';
+import AccordionBox, { AccordionBoxOptions } from '../../../../sun/js/AccordionBox.js';
 import GQColors from '../../common/GQColors.js';
 import GQConstants from '../../common/GQConstants.js';
 import GQCheckbox from '../../common/view/GQCheckbox.js';
 import graphingQuadratics from '../../graphingQuadratics.js';
 import GraphingQuadraticsStrings from '../../GraphingQuadraticsStrings.js';
+import ExploreViewProperties from './ExploreViewProperties.js';
+
+type SelfOptions = EmptySelfOptions;
+
+type QuadraticTermsAccordionBoxOptions = SelfOptions & PickRequired<AccordionBoxOptions, 'tandem' | 'expandedProperty'>;
 
 export default class QuadraticTermsAccordionBox extends AccordionBox {
 
-  /**
-   * @param {ExploreViewProperties} viewProperties
-   * @param {Object} [options]
-   */
-  constructor( viewProperties, options ) {
+  public constructor( viewProperties: ExploreViewProperties, providedOptions: QuadraticTermsAccordionBoxOptions ) {
 
-    options = merge( {}, GQConstants.ACCORDION_BOX_OPTIONS, {
+    const options = optionize4<QuadraticTermsAccordionBoxOptions, SelfOptions, AccordionBoxOptions>()(
+      {}, GQConstants.ACCORDION_BOX_OPTIONS, {
 
-      // AccordionBox options
-      titleAlignX: 'left',
-      titleXSpacing: 8,
-
-      // phet-io
-      tandem: Tandem.REQUIRED,
-      phetioDocumentation: 'the Quadratic Terms accordion box'
-
-    }, options );
+        // AccordionBoxOptions
+        titleAlignX: 'left',
+        titleXSpacing: 8,
+        phetioDocumentation: 'the Quadratic Terms accordion box'
+      }, providedOptions );
 
     // AccordionBox title
-    assert && assert( !options.titleNode, 'QuadraticTermsAccordionBox sets titleNode' );
     options.titleNode = new Text( GraphingQuadraticsStrings.quadraticTerms, {
       font: GQConstants.TITLE_FONT,
       maxWidth: 180, // determined empirically
@@ -64,25 +60,25 @@ export default class QuadraticTermsAccordionBox extends AccordionBox {
     const equationsCheckbox = GQCheckbox.createEquationsCheckbox( viewProperties.equationsVisibleProperty,
       options.tandem.createTandem( 'equationsCheckbox' ) );
 
-    const maxCheckboxWidth = _.maxBy(
-      [ quadraticTermCheckbox, linearTermCheckbox, constantTermCheckbox, equationsCheckbox ],
-      node => node.width ).width;
+    // To make all checkboxes have the same effective width
+    const alignBoxOptions: AlignBoxOptions = {
+      group: new AlignGroup(),
+      xAlign: 'left'
+    };
 
     // vertical layout
     const contentNode = new VBox( {
       align: 'left',
       spacing: GQConstants.CHECKBOXES_Y_SPACING,
       children: [
-        quadraticTermCheckbox,
-        linearTermCheckbox,
-        constantTermCheckbox,
+        new AlignBox( quadraticTermCheckbox, alignBoxOptions ),
+        new AlignBox( linearTermCheckbox, alignBoxOptions ),
+        new AlignBox( constantTermCheckbox, alignBoxOptions ),
         new HSeparator( {
           stroke: GQColors.SEPARATOR,
-
-          // See https://github.com/phetsims/graphing-quadratics/issues/128
-          minimumWidth: Math.max( maxCheckboxWidth, 1.1 * options.titleNode.width )
+          minimumWidth: 1.1 * options.titleNode.width
         } ),
-        equationsCheckbox
+        new AlignBox( equationsCheckbox, alignBoxOptions )
       ]
     } );
 
