534488418a6afd449de9b772cbab6a624641293a
pixelzoom <cmalley@pixelzoom.com>
2023-02-20 16:56:43 -0700

    convert to TypeScript

--- js/standardform/view/StandardFormEquationAccordionBox.ts
+++ js/standardform/view/StandardFormEquationAccordionBox.ts
@@ -1,40 +1,39 @@
 // Copyright 2018-2022, University of Colorado Boulder
 
-// @ts-nocheck
 /**
  * Equation accordion box in the 'Standard Form' screen.
  *
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
-import merge from '../../../../phet-core/js/merge.js';
-import Tandem from '../../../../tandem/js/Tandem.js';
-import GQEquationAccordionBox from '../../common/view/GQEquationAccordionBox.js';
+import optionize, { EmptySelfOptions } from '../../../../phet-core/js/optionize.js';
+import PickRequired from '../../../../phet-core/js/types/PickRequired.js';
+import GQEquationAccordionBox, { GQEquationAccordionBoxOptions } from '../../common/view/GQEquationAccordionBox.js';
 import graphingQuadratics from '../../graphingQuadratics.js';
+import StandardFormModel from '../model/StandardFormModel.js';
 import StandardFormEquationNode from './StandardFormEquationNode.js';
 import StandardFormInteractiveEquationNode from './StandardFormInteractiveEquationNode.js';
 
+type SelfOptions = EmptySelfOptions;
+
+type StandardFormEquationAccordionBoxOptions = SelfOptions &
+  PickRequired<GQEquationAccordionBoxOptions, 'tandem' | 'expandedProperty'>;
+
 export default class StandardFormEquationAccordionBox extends GQEquationAccordionBox {
 
-  /**
-   * @param {StandardFormModel} model
-   * @param {Object} [options]
-   */
-  constructor( model, options ) {
+  public constructor( model: StandardFormModel, providedOptions: StandardFormEquationAccordionBoxOptions ) {
 
-    options = merge( {
-
-      // phet-io
-      tandem: Tandem.REQUIRED,
-      phetioDocumentation: 'accordion box that contains the interactive equation'
-
-    }, options );
-
-    assert && assert( !options.titleNode, 'StandardFormEquationAccordionBox sets titleNode' );
-    options.titleNode = new StandardFormEquationNode( {
-      tandem: options.tandem.createTandem( 'titleText' ),
+    const titleNode = new StandardFormEquationNode( {
+      tandem: providedOptions.tandem.createTandem( 'titleText' ),
       phetioDocumentation: 'the equation shown at the top of this accordion box'
     } );
+
+    const options = optionize<StandardFormEquationAccordionBoxOptions, SelfOptions, GQEquationAccordionBoxOptions>()( {
+
+      // GQEquationAccordionBoxOptions
+      titleNode: titleNode,
+      phetioDocumentation: 'accordion box that contains the interactive equation'
+    }, providedOptions );
 
     const interactiveEquationNode = new StandardFormInteractiveEquationNode(
       model.aProperty, model.bProperty, model.cProperty, {
