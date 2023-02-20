b222949ee68169409ff302b2e457413bfb57c9f5
pixelzoom <cmalley@pixelzoom.com>
2023-02-20 09:44:40 -0700

    convert to TypeScript

--- js/common/view/CoordinatesNode.ts
+++ js/common/view/CoordinatesNode.ts
@@ -1,6 +1,5 @@
 // Copyright 2018-2022, University of Colorado Boulder
 
-// @ts-nocheck
 /**
  * Displays '(x, y)' coordinates.  If the coordinates are null, this displays '(?, ?)'.
  * Performance is optimized to update only when visible.
@@ -8,94 +7,80 @@
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
+import DerivedProperty from '../../../../axon/js/DerivedProperty.js';
+import Property from '../../../../axon/js/Property.js';
 import Utils from '../../../../dot/js/Utils.js';
-import merge from '../../../../phet-core/js/merge.js';
-import { Node, Rectangle, Text } from '../../../../scenery/js/imports.js';
+import Vector2 from '../../../../dot/js/Vector2.js';
+import optionize from '../../../../phet-core/js/optionize.js';
+import PickOptional from '../../../../phet-core/js/types/PickOptional.js';
+import PickRequired from '../../../../phet-core/js/types/PickRequired.js';
+import { Node, NodeOptions, Rectangle, TColor, Text } from '../../../../scenery/js/imports.js';
 import graphingQuadratics from '../../graphingQuadratics.js';
 import GraphingQuadraticsStrings from '../../GraphingQuadraticsStrings.js';
 import GQConstants from '../GQConstants.js';
 
+const X_MARGIN = 4;
+const Y_MARGIN = 2;
+
+type SelfOptions = {
+  decimals?: number;
+  foregroundColor?: TColor;
+  backgroundColor?: TColor;
+};
+
+type CoordinatesNodeOptions = SelfOptions &
+  PickOptional<NodeOptions, 'visibleProperty' | 'phetioDocumentation' | 'pickable'> &
+  PickRequired<NodeOptions, 'tandem'>;
+
 export default class CoordinatesNode extends Node {
 
-  /**
-   * @param {Property.<Vector2|null>} coordinatesProperty
-   * @param {Object} [options]
-   */
-  constructor( coordinatesProperty, options ) {
+  public constructor( coordinatesProperty: Property<Vector2 | null>, providedOptions: CoordinatesNodeOptions ) {
 
-    options = merge( {
+    const options = optionize<CoordinatesNodeOptions, SelfOptions, NodeOptions>()( {
+
+      // SelfOptions
       decimals: 0,
-      font: GQConstants.COORDINATES_FONT,
       foregroundColor: 'black',
       backgroundColor: 'white',
-      backgroundOpacity: 0.75,
-      cornerRadius: 4,
-      xMargin: 4,
-      yMargin: 2,
+
+      // NodeOptions
       maxWidth: 100 // determined empirically
-    }, options );
+    }, providedOptions );
+
+    const foregroundStringProperty = new DerivedProperty(
+      [ coordinatesProperty, GraphingQuadraticsStrings.coordinateUnknownStringProperty ],
+      ( coordinates, coordinateUnknownString ) => {
+        const x = coordinates ? Utils.toFixedNumber( coordinates.x, options.decimals ) : coordinateUnknownString;
+        const y = coordinates ? Utils.toFixedNumber( coordinates.y, options.decimals ) : coordinateUnknownString;
+        return `(${x}, ${y})`;
+      } );
 
     // the coordinates
-    const foregroundNode = new Text( '', {
-      font: options.font,
+    const foregroundNode = new Text( foregroundStringProperty, {
+      font: GQConstants.COORDINATES_FONT,
       fill: options.foregroundColor
     } );
 
     // rectangle behind the coordinates
     const backgroundNode = new Rectangle( 0, 0, 1, 1, {
       fill: options.backgroundColor,
-      opacity: options.backgroundOpacity,
-      cornerRadius: options.cornerRadius
+      opacity: 0.75,
+      cornerRadius: 4
     } );
 
-    assert && assert( !options.children, 'CoordinatesNode sets children' );
     options.children = [ backgroundNode, foregroundNode ];
 
     super( options );
 
-    // @private needed by methods
-    this.coordinatesProperty = coordinatesProperty;
-    this.decimals = options.decimals;
-    this.xMargin = options.xMargin;
-    this.yMargin = options.yMargin;
-    this.foregroundNode = foregroundNode;
-    this.backgroundNode = backgroundNode;
+    foregroundNode.boundsProperty.link( () => {
 
-    coordinatesProperty.link( coordinates => this.update( coordinates ) );
-  }
+      // resize background
+      backgroundNode.setRect( 0, 0,
+        foregroundNode.width + ( 2 * X_MARGIN ), foregroundNode.height + ( 2 * Y_MARGIN ) );
 
-  /**
-   * Sets the foreground color, the color of the coordinates.
-   * @param {Color|string} color
-   * @public
-   */
-  set foreground( color ) { this.foregroundNode.fill = color; }
-
-  /**
-   * Sets the background color, the color of the rectangle behind the coordinates.
-   * @param {Color|string} color
-   * @public
-   */
-  set background( color ) { this.backgroundNode.fill = color; }
-
-  /**
-   * Updates this Node.
-   * @param {Vector2} coordinates
-   * @private
-   */
-  update( coordinates ) {
-
-    // coordinates
-    const x = coordinates ? Utils.toFixedNumber( coordinates.x, this.decimals ) : GraphingQuadraticsStrings.coordinateUnknown;
-    const y = coordinates ? Utils.toFixedNumber( coordinates.y, this.decimals ) : GraphingQuadraticsStrings.coordinateUnknown;
-    this.foregroundNode.text = `(${x}, ${y})`;
-
-    // resize background
-    this.backgroundNode.setRect( 0, 0,
-      this.foregroundNode.width + ( 2 * this.xMargin ), this.foregroundNode.height + ( 2 * this.yMargin ) );
-
-    // center coordinates in background
-    this.foregroundNode.center = this.backgroundNode.center;
+      // center coordinates in background
+      foregroundNode.center = backgroundNode.center;
+    } );
   }
 }
 
