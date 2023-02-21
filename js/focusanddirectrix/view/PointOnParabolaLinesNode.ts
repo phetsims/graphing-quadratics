604f76f3ff0dfede0161e918c1881fee4d0426c5
pixelzoom <cmalley@pixelzoom.com>
2023-02-20 18:00:43 -0700

    convert to TypeScript

--- js/focusanddirectrix/view/PointOnParabolaLinesNode.ts
+++ js/focusanddirectrix/view/PointOnParabolaLinesNode.ts
@@ -1,6 +1,5 @@
 // Copyright 2018-2022, University of Colorado Boulder
 
-// @ts-nocheck
 /**
  * Dashed lines that connect the 'point on parabola' to the focus and directrix.
  * The visibility of the point-focus and point-directrix lines is controlled independently.
@@ -10,23 +9,24 @@
 
 import DerivedProperty from '../../../../axon/js/DerivedProperty.js';
 import Multilink from '../../../../axon/js/Multilink.js';
-import { Line, Node } from '../../../../scenery/js/imports.js';
+import TReadOnlyProperty from '../../../../axon/js/TReadOnlyProperty.js';
+import Vector2 from '../../../../dot/js/Vector2.js';
+import { combineOptions } from '../../../../phet-core/js/optionize.js';
+import ModelViewTransform2 from '../../../../phetcommon/js/view/ModelViewTransform2.js';
+import { Line, LineOptions, Node } from '../../../../scenery/js/imports.js';
 import GQColors from '../../common/GQColors.js';
 import GQConstants from '../../common/GQConstants.js';
+import Quadratic from '../../common/model/Quadratic.js';
 import graphingQuadratics from '../../graphingQuadratics.js';
 
 export default class PointOnParabolaLinesNode extends Node {
 
-  /**
-   * @param {Property.<Quadratic>} quadraticProperty - the interactive quadratic
-   * @param {Vector2Property} pointOnParabolaProperty - the point
-   * @param {ModelViewTransform2} modelViewTransform
-   * @param {Property.<Boolean>} pointOnParabolaVisibleProperty
-   * @param {Property.<Boolean>} focusVisibleProperty
-   * @param {Property.<Boolean>} directrixVisibleProperty
-   */
-  constructor( quadraticProperty, pointOnParabolaProperty, modelViewTransform,
-               pointOnParabolaVisibleProperty, focusVisibleProperty, directrixVisibleProperty ) {
+  public constructor( quadraticProperty: TReadOnlyProperty<Quadratic>,
+                      pointOnParabolaProperty: TReadOnlyProperty<Vector2>,
+                      modelViewTransform: ModelViewTransform2,
+                      pointOnParabolaVisibleProperty: TReadOnlyProperty<boolean>,
+                      focusVisibleProperty: TReadOnlyProperty<boolean>,
+                      directrixVisibleProperty: TReadOnlyProperty<boolean> ) {
 
     const pathOptions = {
       stroke: GQColors.POINT_ON_PARABOLA,
@@ -35,42 +35,42 @@ export default class PointOnParabolaLinesNode extends Node {
     };
 
     // line connecting point and focus
-    const focusLine = new Line( 0, 0, 0, 1, pathOptions );
+    const focusLine = new Line( 0, 0, 0, 1, combineOptions<LineOptions>( {
+      visibleProperty: new DerivedProperty(
+        [ pointOnParabolaVisibleProperty, focusVisibleProperty ],
+        ( pointOnParabolaVisible, focusVisibleProperty ) => {
+          return ( pointOnParabolaVisible && focusVisibleProperty );
+        } )
+    }, pathOptions ) );
 
     // line connecting point and directrix                                             
-    const directrixLine = new Line( 0, 0, 0, 1, pathOptions );
+    const directrixLine = new Line( 0, 0, 0, 1, combineOptions<LineOptions>( {
+      visibleProperty: new DerivedProperty(
+        [ pointOnParabolaVisibleProperty, directrixVisibleProperty ],
+        ( pointOnParabolaVisible, directrixVisible ) => {
+          return ( pointOnParabolaVisible && directrixVisible );
+        } )
+    }, pathOptions ) );
 
-    super( { children: [ focusLine, directrixLine ] } );
+    super( {
+      children: [ focusLine, directrixLine ]
+    } );
 
     // update the lines
     Multilink.multilink( [ quadraticProperty, pointOnParabolaProperty ],
       ( quadratic, pointOnParabola ) => {
 
         assert && assert( quadratic.isaParabola(), `expected a parabola, quadratic=${quadratic}` );
+        assert && assert( quadratic.focus );
+        assert && assert( quadratic.directrix );
 
         const pointView = modelViewTransform.modelToViewPosition( pointOnParabola );
-        const focusView = modelViewTransform.modelToViewPosition( quadratic.focus );
-        const directrixView = modelViewTransform.modelToViewY( quadratic.directrix );
+        const focusView = modelViewTransform.modelToViewPosition( quadratic.focus! );
+        const directrixView = modelViewTransform.modelToViewY( quadratic.directrix! );
 
         focusLine.setLine( pointView.x, pointView.y, focusView.x, focusView.y );
         directrixLine.setLine( pointView.x, pointView.y, pointView.x, directrixView );
       } );
-
-    // visibility of line connecting point and focus
-    const focusLineVisibleProperty = new DerivedProperty(
-      [ pointOnParabolaVisibleProperty, focusVisibleProperty ],
-      ( pointOnParabolaVisible, focusVisibleProperty ) => {
-        return ( pointOnParabolaVisible && focusVisibleProperty );
-      } );
-    focusLineVisibleProperty.linkAttribute( focusLine, 'visible' );
-
-    // visibility of line connecting point and directrix
-    const directrixLineVisibleProperty = new DerivedProperty(
-      [ pointOnParabolaVisibleProperty, directrixVisibleProperty ],
-      ( pointOnParabolaVisible, directrixVisible ) => {
-        return ( pointOnParabolaVisible && directrixVisible );
-      } );
-    directrixLineVisibleProperty.linkAttribute( directrixLine, 'visible' );
   }
 }
 
