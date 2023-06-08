6c64a0b76aebb69b5fdb0545cb3dface7bc703ef
pixelzoom <cmalley@pixelzoom.com>
2023-06-07 18:51:00 -0600

    supportsDynamicLocale: true, grunt modulify, https://github.com/phetsims/graphing-quadratics/issues/178

--- js/GraphingQuadraticsStrings.ts
+++ js/GraphingQuadraticsStrings.ts
@@ -10,56 +10,32 @@ import graphingQuadratics from './graphingQuadratics.js';
 
 type StringsType = {
   'graphing-quadratics': {
-    'title': string;
     'titleStringProperty': LinkableProperty<string>;
   };
   'screen': {
-    'explore': string;
     'exploreStringProperty': LinkableProperty<string>;
-    'standardForm': string;
     'standardFormStringProperty': LinkableProperty<string>;
-    'vertexForm': string;
     'vertexFormStringProperty': LinkableProperty<string>;
-    'focusAndDirectrix': string;
     'focusAndDirectrixStringProperty': LinkableProperty<string>;
   };
-  'vertex': string;
   'vertexStringProperty': LinkableProperty<string>;
-  'axisOfSymmetry': string;
   'axisOfSymmetryStringProperty': LinkableProperty<string>;
-  'roots': string;
   'rootsStringProperty': LinkableProperty<string>;
-  'coordinates': string;
   'coordinatesStringProperty': LinkableProperty<string>;
-  'equations': string;
   'equationsStringProperty': LinkableProperty<string>;
-  'noRealRoots': string;
   'noRealRootsStringProperty': LinkableProperty<string>;
-  'pointOnParabola': string;
   'pointOnParabolaStringProperty': LinkableProperty<string>;
-  'quadraticTerms': string;
   'quadraticTermsStringProperty': LinkableProperty<string>;
-  'directrix': string;
   'directrixStringProperty': LinkableProperty<string>;
-  'focus': string;
   'focusStringProperty': LinkableProperty<string>;
-  'x': string;
   'xStringProperty': LinkableProperty<string>;
-  'y': string;
   'yStringProperty': LinkableProperty<string>;
-  'a': string;
   'aStringProperty': LinkableProperty<string>;
-  'b': string;
   'bStringProperty': LinkableProperty<string>;
-  'c': string;
   'cStringProperty': LinkableProperty<string>;
-  'h': string;
   'hStringProperty': LinkableProperty<string>;
-  'k': string;
   'kStringProperty': LinkableProperty<string>;
-  'p': string;
   'pStringProperty': LinkableProperty<string>;
-  'coordinateUnknown': string;
   'coordinateUnknownStringProperty': LinkableProperty<string>;
 };
 
