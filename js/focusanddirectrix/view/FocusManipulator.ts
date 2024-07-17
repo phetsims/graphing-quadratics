df8671de9a2e96f4973bfc3b240c0bf537c3fb8e
phet-dev <phetadmin@gmail.com>
2024-07-17 03:50:55 -0600

    update copyright dates from daily grunt work

--- js/focusanddirectrix/view/FocusManipulator.ts
+++ js/focusanddirectrix/view/FocusManipulator.ts
@@ -1,4 +1,4 @@
-// Copyright 2018-2023, University of Colorado Boulder
+// Copyright 2018-2024, University of Colorado Boulder
 
 /**
  * FocusManipulator is the manipulator for editing a quadratic (parabola) by changing its focus.
