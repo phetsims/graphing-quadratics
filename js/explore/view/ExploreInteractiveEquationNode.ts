622fca64f02b541f82df6fbd77a9b464219fc4c9
phet-dev <phetadmin@gmail.com>
2024-08-21 03:21:11 -0600

    update copyright dates from daily grunt work

--- js/explore/view/ExploreInteractiveEquationNode.ts
+++ js/explore/view/ExploreInteractiveEquationNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2018-2023, University of Colorado Boulder
+// Copyright 2018-2024, University of Colorado Boulder
 
 /**
  * Standard form equation, y = ax^2 + bx + c, with coefficients that can be changed via sliders.
