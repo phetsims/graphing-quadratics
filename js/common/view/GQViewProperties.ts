d7e5bde8c0f60711b52930cd39c827331c16dc2f
phet-dev <phetadmin@gmail.com>
2025-07-01 03:22:48 -0600

    update copyright dates from daily grunt work

--- js/common/view/GQViewProperties.ts
+++ js/common/view/GQViewProperties.ts
@@ -1,4 +1,4 @@
-// Copyright 2018-2023, University of Colorado Boulder
+// Copyright 2018-2025, University of Colorado Boulder
 
 /**
  * View-specific Properties and properties that are common to more than one screen.
