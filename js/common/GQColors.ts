a01fbee994c68e19cf293262295bc947a7015498
pixelzoom <cmalley@pixelzoom.com>
2025-07-17 14:53:44 -0600

    convert GQColors to class, https://github.com/phetsims/graphing-quadratics/issues/215

--- js/common/GQColors.ts
+++ js/common/GQColors.ts
@@ -15,99 +15,102 @@ import graphingQuadratics from '../graphingQuadratics.js';
 const VERTEX = 'rgb( 128, 0, 128 )'; // purple
 const FOCUS = 'green';
 
-const GQColors = {
+export default class GQColors {
 
+  private constructor() {
+    // Not intended for instantiation.
+  }
+  
   // NOTE: Colors for curves are not using ProfileColorProperty because doing so would require changing the PhET-iO API
   // for QuadraticIO. So unless there's a solid need to have profile support for these colors, leave them as is.
   // See https://github.com/phetsims/graphing-quadratics/issues/204#issuecomment-1730736421
 
   // interactive curves on each screen
-  EXPLORE_INTERACTIVE_CURVE: PhetColorScheme.RED_COLORBLIND,
-  STANDARD_FORM_INTERACTIVE_CURVE: PhetColorScheme.RED_COLORBLIND,
-  VERTEX_FORM_INTERACTIVE_CURVE: 'black',
-  FOCUS_AND_DIRECTRIX_INTERACTIVE_CURVE: 'black',
+  public static readonly EXPLORE_INTERACTIVE_CURVE = PhetColorScheme.RED_COLORBLIND;
+  public static readonly STANDARD_FORM_INTERACTIVE_CURVE = PhetColorScheme.RED_COLORBLIND;
+  public static readonly VERTEX_FORM_INTERACTIVE_CURVE = 'black';
+  public static readonly FOCUS_AND_DIRECTRIX_INTERACTIVE_CURVE = 'black';
 
   // curves common to all screens
-  SAVED_CURVE: 'rgb( 160, 160, 160 )',
+  public static readonly SAVED_CURVE = 'rgb( 160, 160, 160 )';
 
   // quadratic equation terms on the Explore screen
-  QUADRATIC_TERM: 'blue',
-  LINEAR_TERM: 'green',
-  CONSTANT_TERM: 'magenta',
+  public static readonly QUADRATIC_TERM = 'blue';
+  public static readonly LINEAR_TERM = 'green';
+  public static readonly CONSTANT_TERM = 'magenta';
 
   // Coefficients on the Explore screen
-  exploreAColorProperty: new ProfileColorProperty( graphingQuadratics, 'exploreAColor', {
+  public static readonly exploreAColorProperty = new ProfileColorProperty( graphingQuadratics, 'exploreAColor', {
     default: PhetColorScheme.RED_COLORBLIND
-  } ),
-  exploreBColorProperty: new ProfileColorProperty( graphingQuadratics, 'exploreBColor', {
+  } );
+  public static readonly exploreBColorProperty = new ProfileColorProperty( graphingQuadratics, 'exploreBColor', {
     default: PhetColorScheme.RED_COLORBLIND
-  } ),
-  exploreCColorProperty: new ProfileColorProperty( graphingQuadratics, 'exploreCColor', {
+  } );
+  public static readonly exploreCColorProperty = new ProfileColorProperty( graphingQuadratics, 'exploreCColor', {
     default: PhetColorScheme.RED_COLORBLIND
-  } ),
+  } );
 
   // Coefficients on the Standard Form screen
-  standardFormAColorProperty: new ProfileColorProperty( graphingQuadratics, 'standardFormAColor', {
+  public static readonly standardFormAColorProperty = new ProfileColorProperty( graphingQuadratics, 'standardFormAColor', {
     default: PhetColorScheme.RED_COLORBLIND
-  } ),
-  standardFormBColorProperty: new ProfileColorProperty( graphingQuadratics, 'standardFormBColor', {
+  } );
+  public static readonly standardFormBColorProperty = new ProfileColorProperty( graphingQuadratics, 'standardFormBColor', {
     default: PhetColorScheme.RED_COLORBLIND
-  } ),
-  standardFormCColorProperty: new ProfileColorProperty( graphingQuadratics, 'standardFormCColor', {
+  } );
+  public static readonly standardFormCColorProperty = new ProfileColorProperty( graphingQuadratics, 'standardFormCColor', {
     default: PhetColorScheme.RED_COLORBLIND
-  } ),
+  } );
 
   // Coefficients on the Vertex Form screen
-  vertexFormAColorProperty: new ProfileColorProperty( graphingQuadratics, 'vertexFormAColor', {
+  public static readonly vertexFormAColorProperty = new ProfileColorProperty( graphingQuadratics, 'vertexFormAColor', {
     default: FOCUS
-  } ),
-  vertexFormHColorProperty: new ProfileColorProperty( graphingQuadratics, 'vertexFormHColor', {
+  } );
+  public static readonly vertexFormHColorProperty = new ProfileColorProperty( graphingQuadratics, 'vertexFormHColor', {
     default: VERTEX
-  } ),
-  vertexFormKColorProperty: new ProfileColorProperty( graphingQuadratics, 'vertexFormKColor', {
+  } );
+  public static readonly vertexFormKColorProperty = new ProfileColorProperty( graphingQuadratics, 'vertexFormKColor', {
     default: VERTEX
-  } ),
+  } );
 
   // Coefficients on the Focus & Directrix screen
-  focusAndDirectrixPColorProperty: new ProfileColorProperty( graphingQuadratics, 'focusAndDirectrixPColor', {
+  public static readonly focusAndDirectrixPColorProperty = new ProfileColorProperty( graphingQuadratics, 'focusAndDirectrixPColor', {
     default: FOCUS
-  } ),
-  focusAndDirectrixHColorProperty: new ProfileColorProperty( graphingQuadratics, 'focusAndDirectrixHColor', {
+  } );
+  public static readonly focusAndDirectrixHColorProperty = new ProfileColorProperty( graphingQuadratics, 'focusAndDirectrixHColor', {
     default: VERTEX
-  } ),
-  focusAndDirectrixKColorProperty: new ProfileColorProperty( graphingQuadratics, 'focusAndDirectrixKColor', {
+  } );
+  public static readonly focusAndDirectrixKColorProperty = new ProfileColorProperty( graphingQuadratics, 'focusAndDirectrixKColor', {
     default: VERTEX
-  } ),
+  } );
 
   // common to all screens
-  screenBackgroundColorProperty: new ProfileColorProperty( graphingQuadratics, 'screenBackgroundColor', {
+  public static readonly screenBackgroundColorProperty = new ProfileColorProperty( graphingQuadratics, 'screenBackgroundColor', {
     default: 'rgb( 238, 252, 252 )'
-  } ),
-  controlPanelFillProperty: new ProfileColorProperty( graphingQuadratics, 'controlPanelFill', {
+  } );
+  public static readonly controlPanelFillProperty = new ProfileColorProperty( graphingQuadratics, 'controlPanelFill', {
     default: 'rgb( 238, 238, 238 )'
-  } ),
-  separatorStrokeProperty: new ProfileColorProperty( graphingQuadratics, 'separatorStroke', {
+  } );
+  public static readonly separatorStrokeProperty = new ProfileColorProperty( graphingQuadratics, 'separatorStroke', {
     default: 'rgb( 212, 212, 212 )'
-  } ),
-  focusColorProperty: new ProfileColorProperty( graphingQuadratics, 'focusColor', {
+  } );
+  public static readonly focusColorProperty = new ProfileColorProperty( graphingQuadratics, 'focusColor', {
     default: FOCUS
-  } ),
-  directrixColorProperty: new ProfileColorProperty( graphingQuadratics, 'directrixColor', {
+  } );
+  public static readonly directrixColorProperty = new ProfileColorProperty( graphingQuadratics, 'directrixColor', {
     default: FOCUS
-  } ),
-  vertexColorProperty: new ProfileColorProperty( graphingQuadratics, 'vertexColor', {
+  } );
+  public static readonly vertexColorProperty = new ProfileColorProperty( graphingQuadratics, 'vertexColor', {
     default: VERTEX
-  } ),
-  axisOfSymmetryColorProperty: new ProfileColorProperty( graphingQuadratics, 'axisOfSymmetryColor', {
+  } );
+  public static readonly axisOfSymmetryColorProperty = new ProfileColorProperty( graphingQuadratics, 'axisOfSymmetryColor', {
     default: VERTEX
-  } ),
-  rootsColorProperty: new ProfileColorProperty( graphingQuadratics, 'rootsColor', {
+  } );
+  public static readonly rootsColorProperty = new ProfileColorProperty( graphingQuadratics, 'rootsColor', {
     default: '#0071bc'
-  } ),
-  pointOnParabolaColorProperty: new ProfileColorProperty( graphingQuadratics, 'pointOnParabolaColor', {
+  } );
+  public static readonly pointOnParabolaColorProperty = new ProfileColorProperty( graphingQuadratics, 'pointOnParabolaColor', {
     default: PhetColorScheme.RED_COLORBLIND
-  } )
-};
+  } );
+}
 
 graphingQuadratics.register( 'GQColors', GQColors );
-export default GQColors;
