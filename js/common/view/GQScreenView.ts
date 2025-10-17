24b4350db63cae9ccd00542ff14676e22baaa128
pixelzoom <cmalley@pixelzoom.com>
2025-10-17 11:25:01 -0600

    Shift ResetAllButton the right, https://github.com/phetsims/graphing-quadratics/issues/263

--- js/common/view/GQScreenView.ts
+++ js/common/view/GQScreenView.ts
@@ -27,7 +27,7 @@ import GQViewProperties from './GQViewProperties.js';
 import PointToolNode from './PointToolNode.js';
 
 // constants
-const X_SPACING = 15; // between graph and control panels
+const X_SPACING = 22.5; // between graph and control panels
 
 type SelfOptions = EmptySelfOptions;
 
@@ -97,7 +97,7 @@ export default class GQScreenView extends ScreenView {
     } );
 
     // Set maxWidth for each control panel individually
-    const controlPanelMaxWidth = this.layoutBounds.width - graphNode.width - ( 2 * GQConstants.SCREEN_VIEW_X_MARGIN ) - X_SPACING;
+    const controlPanelMaxWidth = this.layoutBounds.width - graphNode.width - ( 2 * GQConstants.SCREEN_VIEW_X_MARGIN ) - ( 2 * X_SPACING );
     affirm( controlPanelMaxWidth > 0, `unexpected controlPanelMaxWidth: ${controlPanelMaxWidth}` );
     equationAccordionBox.maxWidth = controlPanelMaxWidth;
     graphControlPanel.maxWidth = controlPanelMaxWidth;
@@ -117,7 +117,7 @@ export default class GQScreenView extends ScreenView {
 
     // Horizontally center controls in the space to the right of the graph.
     controlsParent.boundsProperty.link( () => {
-      controlsParent.centerX = graphNode.right + X_SPACING + ( controlPanelMaxWidth / 2 );
+      controlsParent.centerX = graphNode.right + ( controlPanelMaxWidth / 2 );
       controlsParent.top = GQConstants.SCREEN_VIEW_Y_MARGIN;
     } );
 
