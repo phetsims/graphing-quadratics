f7156c6f1b7baa7a7f254399bf3b0a61b9ec1cf2
phet-dev <phetadmin@gmail.com>
2026-02-12 03:40:28 -0700

    update copyright dates from daily grunt work

--- js/common/view/PointToolShortcutsSection.ts
+++ js/common/view/PointToolShortcutsSection.ts
@@ -1,4 +1,4 @@
-// Copyright 2025, University of Colorado Boulder
+// Copyright 2025-2026, University of Colorado Boulder
 
 /**
  * PointToolShortcutsSection is the keyboard help section that describes keyboard shortcuts for the point tools.
