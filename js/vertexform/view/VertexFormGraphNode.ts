bf010265926f5f530b286242eb9dd2af8519fe8d
phet-dev <phetadmin@gmail.com>
2026-03-06 04:34:03 -0700

    update copyright dates from daily grunt work

--- js/vertexform/view/VertexFormGraphNode.ts
+++ js/vertexform/view/VertexFormGraphNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2018-2025, University of Colorado Boulder
+// Copyright 2018-2026, University of Colorado Boulder
 
 /**
  * VertexFormGraphNode is the graph for the 'Vertex Form' screen.
